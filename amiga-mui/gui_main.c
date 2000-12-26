50eda56c63f1fa71959085cc5b8ae81db5dbf830
Sebastian Bauer <mail@sebastianbauer.info>
2000-12-26 18:48:37 +0000

    Initialize the new picturebutton subclass

--- amiga-mui/gui_main.c
+++ amiga-mui/gui_main.c
@@ -111,6 +111,7 @@ void all_del(void)
 	{
 		app_del();
 
+		delete_picturebutton_class();
 		delete_simplehtml_class();
 		delete_composeeditor_class();
 		delete_readlist_class();
@@ -142,7 +143,8 @@ int all_init(void)
 		if (create_foldertreelist_class() && create_mailtreelist_class() &&
 				create_addressstring_class() && create_attachmentlist_class() &&
 				create_datatypes_class() && create_transwnd_class() &&
-				create_readlist_class() && create_composeeditor_class() && create_simplehtml_class())
+				create_readlist_class() && create_composeeditor_class() &&
+				create_simplehtml_class() && create_picturebutton_class())
 		{
 			if (app_init())
 			{

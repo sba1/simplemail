4dd9becfeb37610233857652ae8a6dcc97f41d54
Sebastian Bauer <mail@sebastianbauer.info>
2000-12-26 20:24:38 +0000

    Fixed a bug in get_hexadigit()

--- codecs.c
+++ codecs.c
@@ -46,7 +46,7 @@ static int get_hexadigit(char c, int *pval)
 {
 	int val;
 
-	if (c >= '0' && c < '9') val = c - '0';
+	if (c >= '0' && c <= '9') val = c - '0';
 	else if (c >= 'A' && c <= 'F') val = c - 'A' + 10;
 	else if (c >= 'a' && c <= 'f') val = c - 'a' + 10;
 	else return 0;

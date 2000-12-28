556ff7bca6f9c91b837eac6f6bb5fb59464da8da
Sebastian Bauer <mail@sebastianbauer.info>
2000-12-28 17:16:53 +0000

    Added some new tags (load hook) and a new method to the class

--- amiga-mui/simplehtml_mcc.h
+++ amiga-mui/simplehtml_mcc.h
@@ -1,3 +1,8 @@
+/* NOTE: This class is private!! The interface will change in
+ * the future! It's actually not well designed. Only SimpleMail
+ * should use it, for now!
+ */
+
 #ifndef MUI_SIMPLEHTML_MCC_H
 #define MUI_SIMPLEHTML_MCC_H
 
@@ -14,5 +19,31 @@ __regargs void delete_simplehtml_class(void);
 #define MUIA_SimpleHTML_VertScrollbar	(TAG_USER+0x31200003) /* NS. */
 #define MUIA_SimpleHTML_Priv1					(TAG_USER+0x31200004)
 #define MUIA_SimpleHTML_Priv2					(TAG_USER+0x31200005)
+
+
+/* The Hook is called with the hook itself in A0,
+ * the object in register A2 and a struct MUIP_SimpleHTML_LoadHook * in A1
+ * The hook should return 0 if the loading failed (e.g. unsupported transfer protocol)
+ * else another value. In this case the data should be in buffer and buffer_len
+ */
+#define MUIA_SimpleHTML_LoadHook				(TAG_USER+0x31200006)
+
+#define MUIM_SimpleHTML_AllocateMem		(0x456789)
+
+struct MUIP_SimpleHTML_LoadHook
+{
+	STRPTR uri; /* the uri which should be loaded */
+
+	APTR buffer; /* this should be modified by the hook */
+							 /* it must be allocated with the MUIM_SimpleHTML_AllocateMem function */
+							 /* Never access the buffer after the hook finished! */
+	ULONG buffer_len; /* this too */
+};
+
+struct MUIP_SimpleHTML_AllocateMem
+{
+	ULONG MethodID;
+	ULONG Size;
+};
 
 #endif

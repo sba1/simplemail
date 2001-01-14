9a6fff053993dc6024a2df59d19cf50f5d6dc719
Sebastian Bauer <mail@sebastianbauer.info>
2001-01-14 21:21:41 +0000

    Changed the version's date

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
 #define REVISION	2
-#define DATE		"24.12.2000"
+#define DATE		"14.01.2001"
 #define VERS		"SimpleMail 0.2"
-#define VSTRING		"SimpleMail 0.2 (24.12.2000)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.2 (24.12.2000)"
+#define VSTRING		"SimpleMail 0.2 (14.01.2001)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.2 (14.01.2001)"

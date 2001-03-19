2f24766472040994f95cff44a6f059bc0ffae399
Sebastian Bauer <mail@sebastianbauer.info>
2001-03-19 11:09:25 +0000

    Improved the html output

--- text2html.c
+++ text2html.c
@@ -24,7 +24,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "parse.h"
+#include "support_indep.h"
 #include "text2html.h"
+
+#define SIZE_URI 256
+
+static const char legalchars[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@_?+-,.~/%&=:*#";
+
+static int write_uri(char **buffer_ptr, int *buffer_len_ptr, FILE *fh)
+{
+	char uri[SIZE_URI];
+	char *buffer = *buffer_ptr;
+	int buffer_len = *buffer_len_ptr;
+	int i;
+	
+	for (i = 0; buffer_len && *buffer && strchr(legalchars, *buffer) && i < SIZE_URI-1; i++)
+	{
+		uri[i] = *buffer++;
+		buffer_len--;
+	}
+
+	/* The is probably the end of a sentence */
+	if (strchr(".?!", uri[i-1])) --i;
+	uri[i] = 0;
+
+	if (i)
+	{
+		fprintf(fh,"<A HREF=\"%s\">%s</A>",uri,uri);
+	}
+
+	*buffer_ptr = buffer;
+	*buffer_len_ptr = buffer_len;
+
+	return 1;
+}
 
 char *text2html(char *buffer, int buffer_len, int flags)
 {
@@ -34,29 +68,115 @@ char *text2html(char *buffer, int buffer_len, int flags)
 	if ((fh = tmpfile()))
 	{
 		int len;
+		int last_color = 0; /* the color of the current line */
+		int eval_color = 2; /* recheck the color */
+		int initial_color = 1;
 
 		if (flags & TEXT2HTML_BODY_TAG) fputs("<BODY>",fh);
-		if (flags & TEXT2HTML_FIXED_FONT) fputs("<TT>",fh);
+		if (flags & TEXT2HTML_FIXED_FONT) fputs("<FONT SIZE=-1><TT>",fh);
 
 		while (buffer_len)
 		{
-			unsigned char c = *buffer++;
-			buffer_len--;
-			if (c== '<') fputs("&lt;",fh);
-			else if (c== '>') fputs("&gt;",fh);
-			else if (c == 10) fputs("<BR>",fh);
+			if (eval_color)
+			{
+				int buffer2_len = buffer_len;
+				char *buffer2 = buffer;
+				int new_color = 0;
+
+				while (buffer2_len)
+				{
+					unsigned char c = *buffer2++;
+					buffer2_len--;
+					if (c == '>')
+					{
+						if (new_color == 1) new_color = 2;
+						else new_color = 1;
+					} else
+					{
+						if ((new_color == 1 || new_color == 2) && c != ' ') break;
+						if (c==' ' && new_color == 0) break;
+					}
+				}
+
+				if (last_color != new_color)
+				{
+					if (!initial_color) fputs("</FONT>",fh);
+					if (new_color == 1) fputs("<FONT COLOR=\"white\">",fh);
+					else if (new_color == 2) fputs("<FONT COLOR=\"yellow\">",fh);
+					last_color = new_color;
+					initial_color = 0;
+				}
+
+				eval_color = 0;
+			}
+
+
+			if (!mystrnicmp("http:",buffer,5)) write_uri(&buffer, &buffer_len, fh);
+			else if (!mystrnicmp("mailto:",buffer,7)) write_uri(&buffer, &buffer_len, fh);
+			else if (!mystrnicmp("ftp:",buffer,4)) write_uri(&buffer, &buffer_len, fh);
+			else if (!mystrnicmp("https:",buffer,6)) write_uri(&buffer, &buffer_len, fh);
 			else
 			{
-				if (c == 32) {
-					if (*buffer == 32 || flags & TEXT2HTML_NOWRAP) fputs("&nbsp;",fh);
-					else fputc(32,fh);
-				} else {
-				  if (c) fputc(c,fh);
+				unsigned char c;
+				c = *buffer;
+
+				if (c == '@')
+				{
+					/* For email addresses without mailto: */
+					unsigned char *buffer2 = buffer - 1;
+					unsigned char *buffer3;
+					unsigned char c2;
+					int buffer2_len = buffer_len + 1;
+					char *address;
+
+					while ((c2 = *buffer2))
+					{
+						static const char noaliaschars[] = {
+							" ()<>@,;:\\\"[]"};
+
+						if (strchr(noaliaschars,c2)) break;
+						buffer2_len++;
+						buffer2--;
+					}
+
+					if ((buffer3 = parse_addr_spec(buffer2, &address)))
+					{
+						int email_len;
+						fseek(fh,buffer2 - buffer + 1,SEEK_CUR); /* @ sign has not been written yet */
+						buffer_len += buffer - buffer2;
+						buffer -= buffer - buffer2;
+						email_len = buffer3 - buffer;
+						buffer_len -= email_len;
+						buffer = buffer3;
+						fprintf(fh,"<A HREF=\"mailto:%s\">",address);
+						fputs(address,fh);
+						fputs("</A>",fh);
+						free(address);
+						continue;
+					}
+				}
+
+				buffer++;
+				buffer_len--;
+				if (c== '<') fputs("&lt;",fh);
+				else if (c== '>') fputs("&gt;",fh);
+				else if (c == 10)
+				{
+					eval_color = 1;
+					fputs("<BR>",fh);
+				} else
+				{
+					if (c == 32) {
+						if (*buffer == 32 || flags & TEXT2HTML_NOWRAP) fputs("&nbsp;",fh);
+						else fputc(32,fh);
+					} else {
+					  if (c) fputc(c,fh);
+					}
 				}
 			}
 		}
 
-		if (flags & TEXT2HTML_FIXED_FONT) fputs("</TT>",fh);
+		if (flags & TEXT2HTML_FIXED_FONT) fputs("</TT></FONT>",fh);
 		if (flags & TEXT2HTML_ENDBODY_TAG) fputs("</BODY>",fh);
 		if ((len = ftell(fh)))
 		{

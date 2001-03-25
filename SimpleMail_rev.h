27ded70c656541b5d32a58cbe38e874aa0bab93e
Sebastian Bauer <mail@sebastianbauer.info>
2001-03-25 15:58:52 +0000

    Final changes for the 0.5 alpha release

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	4
-#define DATE		"25.2.2001"
-#define VERS		"SimpleMail 0.4"
-#define VSTRING		"SimpleMail 0.4 (25.2.2001)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.4 (25.2.2001)"
+#define REVISION	5
+#define DATE		"25.3.2001"
+#define VERS		"SimpleMail 0.5"
+#define VSTRING		"SimpleMail 0.5 (25.3.2001)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.5 (25.3.2001)"

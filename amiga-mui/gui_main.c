ee4fb3093e567d7c599428b3b9b6700f64bc0cc0
Sebastian Bauer <mail@sebastianbauer.info>
2001-08-26 17:34:16 +0000

    (De)Initialize the new MultiString Class

--- amiga-mui/gui_main.c
+++ amiga-mui/gui_main.c
@@ -50,6 +50,7 @@
 #include "mainwnd.h"
 #include "mailtreelistclass.h"
 #include "muistuff.h"
+#include "multistringclass.h"
 #include "picturebuttonclass.h"
 #include "popupmenuclass.h"
 #include "readlistclass.h"
@@ -122,6 +123,7 @@ void all_del(void)
 	{
 		app_del();
 
+		delete_multistring_class();
 		delete_filterrule_class();
 		delete_configtreelist_class();
 		delete_icon_class();
@@ -161,7 +163,8 @@ int all_init(void)
 				create_readlist_class() && create_composeeditor_class() &&
 				create_simplehtml_class() && create_picturebutton_class() &&
 				create_popupmenu_class() && create_icon_class() && 
-				create_configtreelist_class() && create_filterrule_class())
+				create_configtreelist_class() && create_filterrule_class() &&
+				create_multistring_class())
 		{
 			if (app_init())
 			{

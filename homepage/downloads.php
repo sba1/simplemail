5a1ff5820e78a8ce400a35d4a624023d7b2ff6b7
Sebastian Bauer <mail@sebastianbauer.info>
2001-09-03 15:06:54 +0000

    Homepage and ReadMe updated

--- homepage/downloads.php
+++ homepage/downloads.php
@@ -1,7 +1,7 @@
 <?php
 
 $filename = "simplemail.lha";
-$version = "0.8";
+$version = "0.9";
 
   require_once("language.inc.php");
   require_once("support.inc.php");
--- homepage/links.php
+++ homepage/links.php
@@ -17,6 +17,7 @@
     <LI><A href="http://wuarchive.wustl.edu/pub/aminet/dev/mui/MCC_TextEditor.lha">The TextEditor MCC from Allan Odgaard.</A></LI>
     <LI><A href="http://www.aphaso.de/download/MCC_NList0_90.lha">The NList MCC from Gilles Masson.</A></LI>
     <LI><A href="http://www.aphaso.de/download/MCC_NListtree18.7.lha">The NListtree MCC from Carsten Scholling.</A></LI>
+    <LI><A href="ftp://de.aminet.net/pub/aminet/dev/mui/MCC_Popph.lha">The Pop Placeholder MCC by Marcin Orlowski.</A></LI>
 </UL>
 
 Here you will find the AmiSSL package by Andrija Antonijevic:

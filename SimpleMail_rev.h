7a7a0ad4854d0a5af021827a44ce12edef9e3183
Sebastian Bauer <mail@sebastianbauer.info>
2001-09-10 19:42:26 +0000

    bumped version to 0.10

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	9
-#define DATE		"3.9.2001"
-#define VERS		"SimpleMail 0.9"
-#define VSTRING		"SimpleMail 0.9 (3.9.2001)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.9 (3.9.2001)"
+#define REVISION	10
+#define DATE		"10.9.2001"
+#define VERS		"SimpleMail 0.10"
+#define VSTRING		"SimpleMail 0.10 (10.9.2001)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.10 (10.9.2001)"

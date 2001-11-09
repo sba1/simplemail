94102f10ee5f19b71f809891e1d01c57f847bc33
Hynek Schlawack <hs@ox.cx>
2001-11-09 21:23:40 +0000

    System-dependent printing routines.

---
+++ amiga-mui/sysprint.c
@@ -0,0 +1,79 @@
+/***************************************************************************
+ SimpleMail - Copyright (C) 2000 Hynek Schlawack and Sebastian Bauer
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+***************************************************************************/
+
+/*
+** sysprint.c
+*/
+
+#include "sysprint.h"
+#include "smintl.h"
+#include "support.h"
+
+#include <exec/types.h>
+#include <dos/dos.h>
+#include <proto/dos.h>
+#include <stdlib.h>
+
+int sysprint_print(PrintHandle *ph, char *txt, unsigned long len)
+{
+	int rc = 0;
+
+	/*txt[len]=0;
+	kprintf(txt);*/
+
+	rc = (Write(ph->printer, txt, len) == len);
+
+	if(rc == 0)
+	{
+		sm_request(NULL, _("Failed while sending data to printer!"), _("Okay"));
+	}
+
+
+	return rc;
+}
+
+PrintHandle * sysprint_prepare(void)
+{
+	PrintHandle *rc = NULL;
+
+	rc = malloc(sizeof(PrintHandle));
+	if(rc != NULL)
+	{
+		rc->printer = Open("PRT:", MODE_NEWFILE);
+		if(rc->printer == 0)
+		{
+			free(rc);
+			rc = NULL;
+			sm_request(NULL, _("Can\'t access printer!"), _("Okay"));
+		}
+	}
+
+	return rc;
+}
+
+void sysprint_cleanup(PrintHandle *ph)
+{
+	if(ph != NULL)
+	{
+		if(ph->printer)
+		{
+			Close(ph->printer);
+		}
+		free(ph);
+	}
+}

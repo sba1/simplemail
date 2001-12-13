cf264b91b495708a593135c57f8a1db6f28251e2
Hynek Schlawack <hs@ox.cx>
2001-12-13 20:47:24 +0000

    Added a contribution by Stefan Martens in the links.

--- homepage/links.php
+++ homepage/links.php
@@ -5,62 +5,73 @@
 ?>
 
 <UL>
-    <LI><A href="http://www.sourceforge.net/projects/simplemail/">SimpleMail's project page at SourceForge.</A></LI>
-    <LI><A href="http://lists.sourceforge.net/lists/listinfo/simplemail-dev/">SimpleMail's developer-mailinglist.</A></LI>
-    <li><A href="http://www.freelists.org/list/simplemail-usr">SimpleMails's user-mailinglist.</A> <strong>Joining is explicitly encouraged!</strong></li>
+	<LI><A href="http://www.sourceforge.net/projects/simplemail/">SimpleMail's project page at SourceForge.</A></LI>
+	<LI><A href="http://lists.sourceforge.net/lists/listinfo/simplemail-dev/">SimpleMail's developer-mailinglist.</A></LI>
+	<li><A href="http://www.freelists.org/list/simplemail-usr">SimpleMails's user-mailinglist.</A> <strong>Joining is explicitly encouraged!</strong></li>
 </UL>
 
 <?php echo get_string($LinksMCCText); ?>
 
 <UL>
-    <LI><A href="http://www.p2p-it.dk/duff/MCC_BetterString.LZX">The BetterString MCC from Allan Odgaard.</A></LI>
-    <LI><A href="http://wuarchive.wustl.edu/pub/aminet/dev/mui/MCC_TextEditor.lha">The TextEditor MCC from Allan Odgaard.</A></LI>
-    <LI><A href="http://www.aphaso.de/download/MCC_NList0_90.lha">The NList MCC from Gilles Masson.</A></LI>
-    <LI><A href="http://www.aphaso.de/download/MCC_NListtree18.7.lha">The NListtree MCC from Carsten Scholling.</A></LI>
-    <LI><A href="ftp://de.aminet.net/pub/aminet/dev/mui/MCC_Popph.lha">The Pop Placeholder MCC by Marcin Orlowski.</A></LI>
+	<LI><A href="http://www.p2p-it.dk/duff/MCC_BetterString.LZX">The BetterString MCC from Allan Odgaard.</A></LI>
+	<LI><A href="http://wuarchive.wustl.edu/pub/aminet/dev/mui/MCC_TextEditor.lha">The TextEditor MCC from Allan Odgaard.</A></LI>
+	<LI><A href="http://www.aphaso.de/download/MCC_NList0_90.lha">The NList MCC from Gilles Masson.</A></LI>
+	<LI><A href="http://www.aphaso.de/download/MCC_NListtree18.7.lha">The NListtree MCC from Carsten Scholling.</A></LI>
+	<LI><A href="ftp://de.aminet.net/pub/aminet/dev/mui/MCC_Popph.lha">The Pop Placeholder MCC by Marcin Orlowski.</A></LI>
 </UL>
+<br>
 
+Contributions by our users:
+<ul>
+	<li>
+		Combining SimpleMail with OpenURL or IBrowse by <A href="mailto:info@stefan1200.de ">Stefan Martens</A>. Avaible in
+		<A href="http://amiwonderland.rootcafe.org/stefan1200/stefan1200/english/simplemail.html" target="_blank">
+		English</A> and <A href="http://amiwonderland.rootcafe.org/stefan1200/stefan1200/deutsch/tipps/simplemail.html" target="_blank">
+		German</A>.
+	</li>
+</ul>
+<br>
 Here you will find the AmiSSL package by Andrija Antonijevic:
 <A href="http://tesla.rcub.bg.ac.yu/~antony/AmiSSL/">AmiSSL</A>
 
 <br>
 <center>
-    <TABLE BORDER=1 BGCOLOR="#EEEEEE" CELLPADDING=5 CELLSPACING=0>
-        <TR><TD COLSPAN=12 ALIGN=CENTER><FONT SIZE=-1><B>Rate this site!</B></FONT> </TD></TR>
-        <TR><TD><FONT SIZE=-2>Very bad</FONT> </TD>
-        <TD><FONT SIZE=-2> 
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=1&amp;la=d&amp;id=980292927.62.155.255.22">1</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=2&amp;la=d&amp;id=980292927.62.155.255.22">2</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=3&amp;la=d&amp;id=980292927.62.155.255.22">3</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=4&amp;la=d&amp;id=980292927.62.155.255.22">4</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=5&amp;la=d&amp;id=980292927.62.155.255.22">5</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=6&amp;la=d&amp;id=980292927.62.155.255.22">6</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=7&amp;la=d&amp;id=980292927.62.155.255.22">7</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=8&amp;la=d&amp;id=980292927.62.155.255.22">8</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=9&amp;la=d&amp;id=980292927.62.155.255.22">9</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=10&amp;la=d&amp;id=980292927.62.155.255.22">10</A></FONT>
-        </TD>
-        <TD><FONT SIZE=-2>Very good</FONT> </TD></TR>
-        <TR><TD COLSPAN=12 ALIGN=CENTER><FONT SIZE=-2>
-        <A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl">ALD</A> powered by
-        <A HREF="http://www.amiga-news.de/">amiga-news.de</A> and <A HREF="http://www.innovative-web.de/">Innovative</A></FONT></TD></TR>
-    </TABLE>
+	<TABLE BORDER=1 BGCOLOR="#EEEEEE" CELLPADDING=5 CELLSPACING=0>
+		<TR><TD COLSPAN=12 ALIGN=CENTER><FONT SIZE=-1><B>Rate this site!</B></FONT> </TD></TR>
+		<TR><TD><FONT SIZE=-2>Very bad</FONT> </TD>
+		<TD><FONT SIZE=-2> 
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=1&amp;la=d&amp;id=980292927.62.155.255.22">1</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=2&amp;la=d&amp;id=980292927.62.155.255.22">2</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=3&amp;la=d&amp;id=980292927.62.155.255.22">3</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=4&amp;la=d&amp;id=980292927.62.155.255.22">4</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=5&amp;la=d&amp;id=980292927.62.155.255.22">5</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=6&amp;la=d&amp;id=980292927.62.155.255.22">6</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=7&amp;la=d&amp;id=980292927.62.155.255.22">7</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=8&amp;la=d&amp;id=980292927.62.155.255.22">8</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=9&amp;la=d&amp;id=980292927.62.155.255.22">9</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl?mode=rate&amp;rate=10&amp;la=d&amp;id=980292927.62.155.255.22">10</A></FONT>
+		</TD>
+		<TD><FONT SIZE=-2>Very good</FONT> </TD></TR>
+		<TR><TD COLSPAN=12 ALIGN=CENTER><FONT SIZE=-2>
+		<A HREF="http://www.amiga-news.de/cgi-bin/anwd-db.pl">ALD</A> powered by
+		<A HREF="http://www.amiga-news.de/">amiga-news.de</A> and <A HREF="http://www.innovative-web.de/">Innovative</A></FONT></TD></TR>
+	</TABLE>
 </center>

bd867fb175aa030c29ab17ba63681724e41febdc
Sebastian Bauer <mail@sebastianbauer.info>
2001-12-26 23:55:48 +0000

    New file for different codesets. Actually some wrappers for uft8 and a function to determine the length of a uft8 string

---
+++ codesets.c
@@ -0,0 +1,59 @@
+/***************************************************************************
+ SimpleMail - Copyright (C) 2000 Hynek Schlawack and Sebastian Bauer
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+***************************************************************************/
+
+/*
+** codesets.c
+*/
+
+#include <string.h>
+
+#include "codesets.h"
+
+static const char trailingBytesForUTF8[256] = {
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,4,4,4,4,5,5,5,5
+};
+
+/**************************************************************************
+ Returns the number of characters a uft8 string has. This is not
+ identically with the size of memory is required to hold the string.
+ Please use uftsize() for this.
+**************************************************************************/
+int uft8len(const uft8 *str)
+{
+	int len ;
+	unsigned char c;
+
+	if (!str) return 0;
+	len = 0;
+
+	while ((c = *str++))
+	{
+		len++;
+		str += trailingBytesForUTF8[c];
+	}
+
+	return len;
+}
+
---
+++ codesets.h
@@ -0,0 +1,30 @@
+/***************************************************************************
+ SimpleMail - Copyright (C) 2000 Hynek Schlawack and Sebastian Bauer
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+***************************************************************************/
+
+#ifndef SM__CODESETS_H
+#define SM__CODESETS_H
+
+typedef unsigned char	uft8;
+
+#define uft8size(s) (s)?(strlen(s):0)
+#define uft8cpy(dest,src) ((uft*)strcpy(dest,src))
+#define uft8cat(dest,src) ((uft*)strcat(dest,src))
+
+int uft8len(const uft8 *str);
+
+#endif

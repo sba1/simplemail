9c832029a81e54c8baa88d6d1137f6af769dbd30
Sebastian Bauer <mail@sebastianbauer.info>
2002-09-07 15:20:25 +0000

    Version bump

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	16
-#define DATE		"25.5.2002"
-#define VERS		"SimpleMail 0.16"
-#define VSTRING		"SimpleMail 0.16 (25.5.2002)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.16 (25.5.2002)"
+#define REVISION	17
+#define DATE		"7.9.2002"
+#define VERS		"SimpleMail 0.17"
+#define VSTRING		"SimpleMail 0.17 (7.9.2002)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.17 (7.9.2002)"
--- homepage/downloads.php
+++ homepage/downloads.php
@@ -1,7 +1,7 @@
 <?php
 
 $filename = "simplemail.lha";
-$version = "0.16";
+$version = "0.17";
 
   require_once("language.inc.php");
   require_once("support.inc.php");

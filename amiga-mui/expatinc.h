1a31e0e2d17bc3b2d12512b7b719cd5089aad087
Sebastian Bauer <mail@sebastianbauer.info>
2002-11-28 10:10:53 +0000

    Created new arch depend way of including the expat include file. Added mmakefile.src for AROS (but of course it does not work yet

--- amiga-mui/expatinc.h
+++ amiga-mui/expatinc.h
@@ -1,6 +1,10 @@
 /* This file includes the amiga specific expat files so 
    expat using is transparent */
 
+#ifdef _AROS
+#include <expat.h>
+#else
 #include <expat/expat.h>
 #include <proto/expat.h>
+#endif
 

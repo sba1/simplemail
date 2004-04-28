da8d9165a31375cea0bd3aae26454d612307e12b
Nicolas Sallin <henes@biclodon.com>
2004-04-28 01:36:38 +0000

    everybody can build it now what an ugly hack... I need to think again about it
    Sebastian: I hope you are happy to get a clean addressbook.c again :)

--- amiga-mui/expatinc.h
+++ amiga-mui/expatinc.h
@@ -7,5 +7,30 @@
 #include <exec/types.h>
 #include <expat/expat.h>
 #include <proto/expat.h>
+
+/********************
+ expat.library hack
+ see startup-morphos.c
+*********************/
+#if defined(__MORPHOS__)
+#include <emul/emulregs.h>
+extern struct EmulLibEntry xml_start_tag_trap;
+extern struct EmulLibEntry xml_end_tag_trap;
+extern struct EmulLibEntry xml_char_data_trap;
+#undef XML_SetCharacterDataHandler
+#undef XML_SetElementHandler
+#define XML_SetCharacterDataHandler(__p0, __p1) \
+	LP2NR(96, XML_SetCharacterDataHandler, \
+		XML_Parser , __p0, a0, \
+		XML_CharacterDataHandler , (void *)&__p1##_trap , a1, \
+		, EXPAT_BASE_NAME, 0, 0, 0, 0, 0, 0)
+#define XML_SetElementHandler(__p0, __p1, __p2) \
+	LP3NR(90, XML_SetElementHandler, \
+		XML_Parser , __p0, a0, \
+		XML_StartElementHandler , (void *)&__p1##_trap, a1, \
+		XML_EndElementHandler , (void *)&__p2##_trap, a2, \
+		, EXPAT_BASE_NAME, 0, 0, 0, 0, 0, 0)
+#endif
+
 #endif
 

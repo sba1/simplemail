00e636aa3cfe9f8b768713ea824dcb76d1552bab
Sebastian Bauer <mail@sebastianbauer.info>
2005-01-14 13:06:28 +0000

    hmac_md5.h is a local include file within simplemail

--- hmac_md5.c
+++ hmac_md5.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <sys/types.h>
 
-#include <hmac_md5.h>
+#include "hmac_md5.h"
 
 void hmac_md5(unsigned char * text, int text_len, unsigned char *key, int key_len, unsigned char digest[16])
 /* text     pointer to data stream */

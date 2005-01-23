f96f24238b7ac18c2fb484e565d82cac92061942
Sebastian Bauer <mail@sebastianbauer.info>
2005-01-23 15:41:35 +0000

    Only use __PRETTY_FUNCTION__ when compiling with GNU C

--- debug.h
+++ debug.h
@@ -41,7 +41,11 @@ void __debug_end(void);
 #define SM_DEBUGF(level,x)
 #else
 extern int __debuglevel;
+#ifdef __GNUC__
 #define SM_DEBUGF(level,x) do { if (level <= __debuglevel) { __debug_begin(); __debug_print("%s/%d [%s()] (%s) => ",__FILE__,__LINE__,__PRETTY_FUNCTION__,ARCH_DEBUG_EXTRA); __debug_print x; __debug_end(); }} while (0)
+#else
+#define SM_DEBUGF(level,x) do { if (level <= __debuglevel) { __debug_begin(); __debug_print("%s/%d (%s) => ",__FILE__,__LINE__,ARCH_DEBUG_EXTRA); __debug_print x; __debug_end(); }} while (0)
+#endif
 #define SM_ENTER SM_DEBUGF(20,("Entered function\n"))
 #define SM_LEAVE SM_DEBUGF(20,("Leave function\n"))
 #define SM_RETURN(val,type) do {SM_DEBUGF(20,("Leave (" type ")\n",val)); return (val);} while (0)

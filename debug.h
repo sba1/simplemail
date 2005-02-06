0d51324187a31ebaa9f2cf4bd44a220f6146029d
Bernd Gollesch <bgollesch@speed.at>
2005-02-06 18:33:33 +0000

    Added __FUNC__ for SASC instead of __PRETTY_FUNCTION__

--- debug.h
+++ debug.h
@@ -44,7 +44,11 @@ extern int __debuglevel;
 #ifdef __GNUC__
 #define SM_DEBUGF(level,x) do { if (level <= __debuglevel) { __debug_begin(); __debug_print("%s/%d [%s()] (%s) => ",__FILE__,__LINE__,__PRETTY_FUNCTION__,ARCH_DEBUG_EXTRA); __debug_print x; __debug_end(); }} while (0)
 #else
+#ifdef __SASC
+#define SM_DEBUGF(level,x) do { if (level <= __debuglevel) { __debug_begin(); __debug_print("%s/%d [%s()] (%s) => ",__FILE__,__LINE__,__FUNC__,ARCH_DEBUG_EXTRA); __debug_print x; __debug_end(); }} while (0)
+#else
 #define SM_DEBUGF(level,x) do { if (level <= __debuglevel) { __debug_begin(); __debug_print("%s/%d (%s) => ",__FILE__,__LINE__,ARCH_DEBUG_EXTRA); __debug_print x; __debug_end(); }} while (0)
+#endif
 #endif
 #define SM_ENTER SM_DEBUGF(20,("Entered function\n"))
 #define SM_LEAVE SM_DEBUGF(20,("Leave function\n"))

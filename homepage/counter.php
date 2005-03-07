4ecf9d3b109598f3b6a8d59877b5d92ae62371c7
Sebastian Bauer <mail@sebastianbauer.info>
2005-03-07 17:22:24 +0000

    Counter should be safer now (it always was reseted in some situations).

--- homepage/counter.php
+++ homepage/counter.php
@@ -1,28 +1,54 @@
 <?php
 
-    if($newguest == "")
-    {
-        setcookie("newguest","true");
-        $newguest="true";
-    }
-    else
-    {
-        setcookie("newguest", "false");
-        $newguest="false";
-    }
+   function counterWrite($file, $increase, $lockfile = null) {
+       // ceate a lock file - if not possible someone else is active so wait (a while)
+       $lock = ($lockfile)? $lockfile : $file.'.lock';
+       if (file_exists($lock))
+	   {
+   		  if (time()-filemtime($lock)>10)
+          unlink($lock);
+       } 
 
-    $fn = "hits.txt";
+       $lf = @fopen ($lock, 'x');
+       while ($lf === FALSE && $i++ < 20) {
+           clearstatcache();
+           usleep(rand(5,85));
+           $lf = @fopen ($lock, 'x');
+       }
+  
+       $hits = 1;
 
-    $fp = fopen($fn,"r");
-    if($fp==0) die("Error while reading hits.txt.");
-    $hits=intval(fread($fp,filesize($fn)),10);
-    fclose($fp);
-    if($newguest == "true")
-    {
-        $hits++;
-        $fp = fopen($fn, "w");
-        fwrite($fp, strval($hits));
-        fclose($fp);
-    }
+       // if lockfile (finally) has been created, file is ours, else give up ...
+       if ($lf !== False)
+       {
+           $fp = @fopen($file, 'r');
+           if ($fp == null)
+	         $fp = @fopen($file.".tmp", 'r');
+           $hits = intval(fgets($fp));
+		   fclose ($fp);
+
+		   if ($increase)
+		   {
+			   $hits++;
+			   $fp = fopen($file.".tmp", 'w');
+    	       fwrite($fp, $hits);
+        	   fclose($fp);
+        	   unlink($file);
+        	   rename($file.".tmp",$file);
+		   }
+
+           // and unlock
+           fclose($lf);
+           unlink($lock);
+       }
+       return $hits;
+   }
+
+	if (!isset($_COOKIE["visited"]))
+	{
+        setcookie("visited","true");
+        $visited = 0;
+    } else $visited = 1;
+
+	$hits = counterWrite("hits.txt", $visited == 0);
 ?>
-

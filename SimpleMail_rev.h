b07d34862953af7c703513bce9a876de2c66eb18
Sebastian Bauer <mail@sebastianbauer.info>
2005-12-24 13:23:33 +0000

    Revison bump.

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	27
-#define DATE		"17.4.2005"
-#define VERS		"SimpleMail 0.27"
-#define VSTRING		"SimpleMail 0.27 (17.4.2005)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.27 (17.4.2005)"
+#define REVISION	28
+#define DATE		"24.12.2005"
+#define VERS		"SimpleMail 0.28"
+#define VSTRING		"SimpleMail 0.28 (24.12.2005)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.28 (24.12.2005)"

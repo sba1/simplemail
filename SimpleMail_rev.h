675202b52dbec466110aaf062fd76a66d210628a
Sebastian Bauer <mail@sebastianbauer.info>
2007-02-04 23:54:27 +0000

    Bumped version. History update.

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	30
-#define DATE		"24.24.2006"
-#define VERS		"SimpleMail 0.30"
-#define VSTRING		"SimpleMail 0.30 (24.12.2006)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.30 (24.12.2006)"
+#define REVISION	31
+#define DATE		"5.2.2007"
+#define VERS		"SimpleMail 0.31"
+#define VSTRING		"SimpleMail 0.31 (5.2.2007)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.31 (5.2.2007)"

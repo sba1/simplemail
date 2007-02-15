42683e2cd1a15fb7d622d4135e3c4e57a5be08e2
Sebastian Bauer <mail@sebastianbauer.info>
2007-02-15 21:42:39 +0000

    Seperated the icon description puposes from dt_node so we can independently free these objects. Added preliminary support for mason icons (note that due to the alpha channel used therein they look ugly, not least as put_dt_on_rasport() doesn't support alphachannel yet). The name of mason icons can be specified in the images.list via a new last column.

--- amiga-mui/datatypescache.c
+++ amiga-mui/datatypescache.c
@@ -36,6 +36,7 @@
 #include <proto/intuition.h>
 
 #include "lists.h"
+#include "debug.h"
 #include "support_indep.h"
 
 #include "amigasupport.h"
@@ -43,30 +44,57 @@
 /**************************************************************/
 
 static Object *img_object;
+static struct Screen *img_screen;
 static int img_usage_count;
+
+static int mason_available;
+
+static struct list desc_list;
 static struct list dt_list;
 
 /**************************************************************/
 
-struct dt_node
+/* An icon description */
+struct icon_desc
 {
 	struct node node;
 	char *filename;
-	Object *o;
-	struct Screen *scr;
-	int count;
+	char *masonname;
 
+	/* Coordinates within the big image */
 	int x1,x2,y1,y2;
 };
+
+struct dt_node
+{
+	struct node node;
+	char *name;
+	struct icon_desc *desc;
+	int count;
+
+	Object *o;
+	int x1,x2,y1,y2;
+	struct Screen *scr;
+};
+
+/**************************************************************/
+
+#ifndef __AMIGAOS4__
+APTR SetProcWindow(void *newvalue)
+{
+	struct Process *myproc = (struct Process *)FindTask(NULL);
+	APTR oldwindowptr = myproc->pr_WindowPtr;
+	myproc->pr_WindowPtr = (APTR)newvalue;
+}
+#endif
 
 Object *LoadPicture(char *filename, struct Screen *scr)
 {
 	Object *o;
+	APTR oldwindowptr;
 
 	/* tell DOS not to bother us with requesters */
-	struct Process *myproc = (struct Process *)FindTask(NULL);
-	APTR oldwindowptr = myproc->pr_WindowPtr;
-	myproc->pr_WindowPtr = (APTR)-1;
+	oldwindowptr = SetProcWindow((APTR)-1);
 
 	o = NewDTObject(filename,
 			DTA_GroupID          , GID_PICTURE,
@@ -77,7 +105,7 @@ Object *LoadPicture(char *filename, struct Screen *scr)
 			PDTA_UseFriendBitMap , TRUE,
 			TAG_DONE);
 	
-	myproc->pr_WindowPtr = oldwindowptr;
+	SetProcWindow(oldwindowptr);
 	
 	/* do all the setup/layout stuff that's necessary to get a bitmap from the dto    */
 	/* note that when using V43 datatypes, this might not be a real "struct BitMap *" */
@@ -102,8 +130,20 @@ Object *LoadPicture(char *filename, struct Screen *scr)
 void dt_init(void)
 {
 	BPTR file;
+	BPTR lock;
+	APTR oldwindowptr;
 
+	list_init(&desc_list);
 	list_init(&dt_list);
+
+	/* Test for general mason availability */
+	oldwindowptr = SetProcWindow((APTR)-1);
+	if ((lock = Lock("TBIMAGES:",ACCESS_READ)))
+	{
+		mason_available = 1;
+		UnLock(lock);
+	}
+	SetProcWindow(oldwindowptr);
 
 	if ((file = Open("PROGDIR:Images/images.list",MODE_OLDFILE)))
 	{
@@ -112,16 +152,29 @@ void dt_init(void)
 		{
 			char *filename_end;
 			char *filename;
+
 			if (*buf==';') continue;
+
+			/* trim ending white spaces */
+			{
+				int buf_len;
+				
+				buf_len = strlen(buf);
+				while (buf_len && isspace((unsigned char)buf[buf_len-1]))
+					buf_len--;
+
+				buf[buf_len] = 0;
+			}
+
 			if ((filename_end = strchr(buf,',')))
 			{
 				if ((filename = malloc(filename_end-buf+50)))
 				{
-					struct dt_node *node = malloc(sizeof(struct dt_node));
+					struct icon_desc *node = malloc(sizeof(struct icon_desc));
 					if (node)
 					{
 						char *lastchar;
-						memset(node,0,sizeof(struct dt_node));
+						memset(node,0,sizeof(struct icon_desc));
 						node->filename = filename;
 						strcpy(filename,"PROGDIR:Images/"); /* 15 chars */
 						strncpy(&filename[15],buf,filename_end-buf);
@@ -130,7 +183,19 @@ void dt_init(void)
 						node->y1 = strtol(lastchar+1,&lastchar,10);
 						node->x2 = strtol(lastchar+1,&lastchar,10);
 						node->y2 = strtol(lastchar+1,&lastchar,10);
-						list_insert_tail(&dt_list,&node->node);
+
+						/* Optional mason name */
+						if (*lastchar == ',')
+						{
+							char *masonname = malloc(strlen(lastchar) + 12);
+							if (masonname)
+							{
+								strcpy(masonname,"TBIMAGES:");
+								strcat(masonname,lastchar+1);
+								node->masonname = masonname;
+							}
+						}
+						list_insert_tail(&desc_list,&node->node);
 					}
 				}
 			}
@@ -142,35 +207,32 @@ void dt_init(void)
 
 void dt_cleanup(void)
 {
-	struct dt_node *node;
-	while ((node = (struct dt_node*)list_remove_tail(&dt_list)))
+	struct dt_node *dt;
+	struct icon_desc *icon;
+	
+	while ((dt = (struct dt_node*)list_remove_tail(&dt_list)))
 	{
-		if (node->o) DisposeDTObject(node->o);
-		if (node->filename) free(node->filename);
-		free(node);
+		if (dt->o) DisposeDTObject(dt->o);
+		free(dt);
 	}
+	
+	while ((icon = (struct icon_desc*)list_remove_tail(&dt_list)))
+	{
+		if (icon->filename) free(icon->filename);
+		if (icon->masonname) free(icon->masonname);
+		free(icon);
+	}
+
 	if (img_object) DisposeDTObject(img_object);
 	img_object = NULL;
 }
 
-struct dt_node *dt_load_picture(char *filename, struct Screen *scr)
+/****************************************************************
+ Create the dt object directly from filename
+****************************************************************/
+static struct dt_node *dt_create_from_filename(char *filename, struct Screen *scr)
 {
-	struct dt_node *node = (struct dt_node*)list_first(&dt_list);
-	while (node)
-	{
-		if (!mystricmp(filename,node->filename))
-		{
-			if (!node->o)
-			{
-				if (!img_object) img_object = LoadPicture("PROGDIR:Images/images",scr);
-				if (!img_object) break;
-				img_usage_count++;
-			}
-			node->count++;
-			return node;
-		}
-		node = (struct dt_node*)node_next(&node->node);
-	}
+	struct dt_node *node;
 
 	if ((node = (struct dt_node*)malloc(sizeof(struct dt_node))))
 	{
@@ -190,7 +252,6 @@ struct dt_node *dt_load_picture(char *filename, struct Screen *scr)
 			}
 			node->scr = scr;
 
-			list_insert_tail(&dt_list,&node->node);
 			return node;
 		}
 		free(node);
@@ -198,21 +259,117 @@ struct dt_node *dt_load_picture(char *filename, struct Screen *scr)
 	return NULL;
 }
 
+/****************************************************************
+ Load the dt object. A given filename is instanciated only
+ once. 
+****************************************************************/
+struct dt_node *dt_load_picture(char *filename, struct Screen *scr)
+{
+	struct icon_desc *icon; 
+	struct dt_node *node;
+
+	SM_ENTER;
+
+	node = (struct dt_node*)list_first(&dt_list);
+	while (node)
+	{
+		if (!mystricmp(filename,node->name))
+		{
+			node->count++;
+			return node;
+		}
+		node = (struct dt_node*)node_next(&node->node);
+	}
+
+	/* Try if we can match an icon description first */
+	icon = (struct icon_desc*)list_first(&desc_list);
+	while (icon)
+	{
+		if (!mystricmp(filename,icon->filename))
+		{
+			/* We can, now check if we can find the mason icon */
+			if (mason_available)
+			{
+				SM_DEBUGF(20,("Trying to load mason icon \"%s\"\n",icon->masonname));
+				if ((node = dt_create_from_filename(icon->masonname,scr)))
+				{
+					SM_DEBUGF(20,("Mason found\n"));
+					SM_RETURN(node,"%p");
+				}
+				SM_DEBUGF(20,("Mason not found\n"));
+			}
+
+			/* No, so we use the big image */
+			if (img_object && img_screen != scr)
+			{
+				/* This has debugging purposes only, it should never happen */
+				SM_DEBUGF(1,("The given screen differs from the screen for which the icon image was loaded for!\n"));
+				img_object = NULL;
+				img_screen = NULL; 
+			}
+			if (!img_object)
+			{
+				img_object = LoadPicture("PROGDIR:Images/images",scr);
+				img_screen = scr;
+			}
+			if (!img_object) break;
+			img_usage_count++;
+
+			if (!(node = malloc(sizeof(struct dt_node))))
+			{
+				memset(node,0,sizeof(struct dt_node));
+				node->count = 1;
+				node->desc = icon;
+				node->name = filename;
+				node->x1 = node->desc->x1;
+				node->x2 = node->desc->x2;
+				node->y1 = node->desc->y1;
+				node->y2 = node->desc->y2;												
+			}
+		}
+		icon = (struct icon_desc*)node_next(&icon->node);
+	}
+
+	/* Try loading the specified image as it is */
+	node = dt_create_from_filename(filename,scr);
+	list_insert_tail(&dt_list,&node->node);
+	SM_RETURN(node,"%p");
+}
+
+/****************************************************************
+ Dispose the given dt
+****************************************************************/
 void dt_dispose_picture(struct dt_node *node)
 {
+	SM_ENTER;
+
 	if (node && node->count)
 	{
 		node->count--;
-		if (!node->o)
+		if (!node->count)
 		{
-			img_usage_count--;
-			if (!img_usage_count)
+			if (node->o)
 			{
-				DisposeDTObject(img_object);
-				img_object = NULL;
-			}
+				SM_DEBUGF(20,("Disposing dt object at %p\n",img_object));
+				DisposeDTObject(node->o);
+			} else
+			{
+				SM_DEBUGF(20,("Disposing dt object icon image at %p\n",img_object));
+				img_usage_count--;
+				if (!img_usage_count)
+				{
+					DisposeDTObject(img_object);
+					img_object = NULL;
+					img_screen = NULL;
+				}
+			}	
+
+			node_remove(&node->node);
+			free(&node->node);
 		}
 	}
+
+	SM_LEAVE;
 }
 
 int dt_width(struct dt_node *node)

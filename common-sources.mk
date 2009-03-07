feb6e688f083f75423c87841dde87bf1a829cb81
Sebastian Bauer <mail@sebastianbauer.info>
2009-03-07 16:37:09 +0000

    Moved the definition of common sources into an own file.

---
+++ common-sources.mk
@@ -0,0 +1,40 @@
+# This make file contains the current set of common sources,
+# i.e., sources that are not specific for a platform
+
+NONARCHSRCS=\
+	account.c \
+	addressbook.c \
+	boyermoore.c \
+	codecs.c \
+	codesets.c \
+	configuration.c \
+	dbx.c \
+	debug.c \
+	estimate.c \
+	filter.c \
+	folder.c \
+	hash.c \
+	hmac_md5.c \
+	http.c \
+	imap.c \
+	lists.c \
+	mail.c \
+	mbox.c \
+	md5.c \
+	parse.c \
+	pgp.c \
+	phrase.c \
+	pop3.c \
+	print.c \
+	punycode.c \
+	signature.c \
+	simplemail.c \
+	smintl.c \
+	smtp.c \
+	spam.c \
+	status.c \
+	support_indep.c \
+	taglines.c \
+	tcp.c \
+	text2html.c \
+	trans.c

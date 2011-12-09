9980e7bc3ed2a2d5007b69b546fe25ddec72ed34
Sebastian Bauer <mail@sebastianbauer.info>
2011-12-09 06:35:53 +0000

    Added a test which crashes here.

--- tests/imap_unittest.c
+++ tests/imap_unittest.c
@@ -2,13 +2,35 @@
  * @file imap_unittest.c
  */
 
+#include "debug.h"
+#include "folder.h"
 #include "imap.h"
 #include "simplemail.h"
 
+#include <proto/dos.h>
+
 int main(int argc, char **argv)
 {
+	struct folder *f;
+
 	if (!simplemail_init())
 		return 20;
+
+	/* We assume that this folder is present and is a IMAP folder */
+	f = folder_find_by_name("INBOX");
+	if (f && f->imap_server)
+	{
+		debug_set_level(20);
+
+		imap_thread_connect(f);
+		Delay(200);
+
+		imap_thread_connect(f);
+		Delay(200);
+	} else
+	{
+		Printf("Folder INBOX not found\n");
+	}
 
 	simplemail_deinit();
 	return 0;

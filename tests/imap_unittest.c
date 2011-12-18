3cd59c0720e5f84211b367eecc0798f7f892a824
Sebastian Bauer <mail@sebastianbauer.info>
2011-12-18 16:40:57 +0000

    Improved test.

--- tests/imap_unittest.c
+++ tests/imap_unittest.c
@@ -4,21 +4,28 @@
 
 #include "debug.h"
 #include "folder.h"
+#include "gui_main.h"
 #include "imap.h"
 #include "progmon.h"
 #include "simplemail.h"
 
 #include <proto/dos.h>
 
-static void test_progmon(void)
+
+static int test_progmon(void *udata)
 {
 	struct progmon *pm;
 	int i;
 
-	if (!(pm = progmon_create()))
-		return;
+	char *name = (char*)udata;
 
-	pm->begin(pm,100,"Test progress");
+	if (thread_get() != thread_get_main())
+		thread_parent_task_can_contiue();
+
+	if (!(pm = progmon_create()))
+		return 0;
+
+	pm->begin(pm,100,name);
 	for (i=0;i<100;i++)
 	{
 		pm->work(pm,1);
@@ -27,6 +34,8 @@ static void test_progmon(void)
 	pm->done(pm);
 
 	progmon_delete(pm);
+
+	return 0;
 }
 
 int main(int argc, char **argv)
@@ -36,13 +45,16 @@ int main(int argc, char **argv)
 	if (!simplemail_init())
 		return 20;
 
+	debug_set_level(20);
+
 	/* We assume that this folder is present and is a IMAP folder */
 	f = folder_find_by_name("INBOX");
 	if (f && f->imap_server)
 	{
-		debug_set_level(20);
+		thread_add("Test1", test_progmon, "Test1");
+		thread_add("Test2", test_progmon, "Test2");
 
-		test_progmon();
+		gui_loop();
 
 		imap_thread_connect(f);
 		Delay(200);

99274ea4797ece2c1d74e56150c8ca966f3b3fe7
Sebastian Bauer <mail@sebastianbauer.info>
2012-12-10 15:35:31 +0000

    Added empty index source file.

---
+++ index.c
@@ -0,0 +1,22 @@
+/**
+ * @file index.c
+ */
+
+#include <string.h>
+
+struct index *index_create(char *filename)
+{
+	return NULL;
+}
+
+void index_delete(struct index *index)
+{
+}
+
+void index_put_text(struct index *index, char *string, int id)
+{
+}
+
+void index_lockup(struct index *index, char *string)
+{
+}

75519816e9f364b84636744facd62a19bf0bcf3e
Sebastian Bauer <mail@sebastianbauer.info>
2012-12-12 16:13:45 +0000

    Added simple unit test for filter_rule_create_from_common_sorted_recipients().

--- tests/filter_unittest.c
+++ tests/filter_unittest.c
@@ -22,6 +22,7 @@
 void test_filter_rule_create_from_strings(void)
 {
 	struct filter *f;
+	struct filter_rule *fr;
 
 	char *subjects[]={
 			"[simplemail] test1",
@@ -29,7 +30,6 @@ void test_filter_rule_create_from_strings(void)
 			"Re: [simplemail] test2",
 			"[simplemail] Re: test2"
 	};
-	struct filter_rule *fr;
 
 	fr = filter_rule_create_from_strings(subjects,4,RULE_SUBJECT_MATCH);
 
@@ -43,5 +43,25 @@ void test_filter_rule_create_from_strings(void)
 
 	filter_add_rule(f,fr);
 
+	filter_dispose(f);
+}
+
+/* @Test */
+void test_filter_rule_create_from_common_sorted_recipients(void)
+{
+	struct filter *f;
+	struct filter_rule *fr;
+	char *a1[] = {"abcdef@eeee","bcdef@eeee", NULL};
+	char *a2[] = {"bcdef@eeee","zzzz@eeee", NULL};
+	char **addresses[] = {a1,a2};
+
+	fr = filter_rule_create_from_common_sorted_recipients(addresses,2);
+	CU_ASSERT(fr != NULL);
+	CU_ASSERT(fr->type == RULE_RCPT_MATCH);
+	CU_ASSERT(fr->u.rcpt.rcpt[0] != NULL);
+	CU_ASSERT(strcmp(fr->u.rcpt.rcpt[0],"bcdef@eeee")==0);
+
+	f = filter_create();
+	CU_ASSERT(f != NULL);
 	filter_dispose(f);
 }

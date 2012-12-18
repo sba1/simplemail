7f94fcc2d33430761db8e02e7db559973847d769
Sebastian Bauer <mail@sebastianbauer.info>
2012-12-18 09:57:46 +0000

    Added test skeleton for string index.

---
+++ tests/index_unittest.c
@@ -0,0 +1,35 @@
+/**
+ * index_unittest - a simple test for the simple string index interface for SimpleMail.
+ * Copyright (C) 2012  Sebastian Bauer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include <CUnit/Basic.h>
+
+#include "index.h"
+#include "index_naive.h"
+
+/*******************************************************/
+
+/* @Test */
+void test_index_naive(void)
+{
+
+}

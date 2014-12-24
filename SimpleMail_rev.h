126ad6a5bce6cdcdd963d321488ad4b533653c4c
Sebastian Bauer <mail@sebastianbauer.info>
2014-12-24 22:41:54 +0100

    Added VERSION-TAG file to indicate a version tag.
    It should be set to SNAPSHOT, if it is no release. If we are in a
    release commit, the file should be empty.
    
    Bumped revsion as we've just released something.

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	41
+#define REVISION	42
 #define DATE		"24.12.2014"
-#define VERS		"SimpleMail 0.41"
-#define VSTRING		"SimpleMail 0.41 (24.12.2014)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.41 (24.12.2014)"
+#define VERS		"SimpleMail 0.42"
+#define VSTRING		"SimpleMail 0.42 (24.12.2014)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.42 (24.12.2014)"

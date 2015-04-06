41d79c377f64e56c4384e8fac58d496d0d74e312
Sebastian Bauer <mail@sebastianbauer.info>
2015-04-06 19:51:22 +0200

    Use proper @file tag.

--- filter.c
+++ filter.c
@@ -16,9 +16,9 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ***************************************************************************/
 
-/*
-** filter.c
-*/
+/**
+ * @file filter.c
+ */
 
 #include "filter.h"
 

f4dfcaaa2359d8478250f4cc3730784456c845e0
Sebastian Bauer <mail@sebastianbauer.info>
2015-05-19 21:53:39 +0200

    Added test_folder_many_mails() test.

--- tests/folder_unittest.c
+++ tests/folder_unittest.c
@@ -41,3 +41,45 @@ void test_folder(void)
 }
 
 /*************************************************************/
+
+#define MANY_EMAILS_PROFILE "/tmp/sm-many-emails-profile"
+
+/* @Test */
+void test_folder_many_mails(void)
+{
+	struct folder *f;
+	char mail_filename[100];
+	int i;
+
+	system("rm -Rf " MANY_EMAILS_PROFILE);
+	config_set_user_profile_directory(MANY_EMAILS_PROFILE);
+
+	CU_ASSERT(load_config() != 0);
+	CU_ASSERT(codesets_init() != 0);
+	CU_ASSERT(init_folders() != 0);
+
+	for (i=0;i<5000;i++)
+	{
+		FILE *fp;
+		struct composed_mail cm;
+		char subject[100];
+
+		snprintf(mail_filename, sizeof(mail_filename), MANY_EMAILS_PROFILE "/.folders/incoming/mail%06d", i);
+		snprintf(subject, sizeof(subject), "Subject %d", i);
+		composed_mail_init(&cm);
+
+		CU_ASSERT((fp = fopen(mail_filename, "w")) != NULL);
+		cm.from = "Sebastian Bauer <mail@sebastianbauer.info>";
+		cm.subject = subject;
+		cm.text = "Hello\n";
+		private_mail_compose_write(fp, &cm);
+		fclose(fp);
+	}
+
+	CU_ASSERT(folder_rescan(folder_incoming(), NULL) != 0);
+	CU_ASSERT(folder_save_index(folder_incoming()) != 0);
+
+	del_folders();
+	codesets_cleanup();
+	free_config();
+}

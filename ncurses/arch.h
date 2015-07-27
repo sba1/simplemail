40bc40ea7e4d8fe91d2b32618f3fe367c3ec22c7
Sebastian Bauer <mail@sebastianbauer.info>
2015-07-27 20:15:10 +0200

    Added makefile and simple files for ncurses version.

---
+++ ncurses/arch.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+ SimpleMail - Copyright (C) 2000 Hynek Schlawack and Sebastian Bauer
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+***************************************************************************/
+
+/*
+** arch.h - architecture specific definitions
+*/
+
+#ifndef SM__ARCH_H
+#define SM__ARCH_H
+
+/* Paths */
+#define SM_DIR		""
+#define SM_CHARSET_DIR	"Charsets"
+#define SM_FOLDER_DIR	".folders"
+#define SM_CURRENT_DIR	"."
+
+/* Operation system */
+#define SM_OPERATINGSYSTEM "UNIX/GTK+"
+#endif
+
+/* Debug - defines ARCH_DEBUG_EXTRA */
+#define ARCH_DEBUG_EXTRA ""

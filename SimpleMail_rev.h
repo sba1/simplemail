8118502aa750b31da71b8f6441dff50d54ccc11a
Sebastian Bauer <mail@sebastianbauer.info>
2015-12-25 10:03:50 +0100

    Work on release 0.43 starts.

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	42
-#define DATE		"24.12.2014"
-#define VERS		"SimpleMail 0.42"
-#define VSTRING		"SimpleMail 0.42 (24.12.2014)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.42 (24.12.2014)"
+#define REVISION	43
+#define DATE		"24.12.2015"
+#define VERS		"SimpleMail 0.43"
+#define VSTRING		"SimpleMail 0.43 (24.12.2015)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.43 (24.12.2015)"

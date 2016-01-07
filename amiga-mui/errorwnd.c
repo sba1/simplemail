9324a871f773fc799473e315d4046c9123c2de50
Sebastian Bauer <mail@sebastianbauer.info>
2016-01-07 21:51:51 +0100

    Use the logg update callback to update the log view.

--- amiga-mui/errorwnd.c
+++ amiga-mui/errorwnd.c
@@ -57,103 +57,7 @@ static Object *all_errors_list;
 static Object *text_list;
 static Object *clear_button;
 
-/**
- * Callback for selecting the currently displayed error message.
- */
-static void error_select(void)
-{
-	int num = xget(all_errors_list, MUIA_NList_Active);
-	struct error_node *node = (struct error_node *)list_find(&error_list,num);
-
-	set(text_list, MUIA_NList_Quiet, TRUE);
-	DoMethod(text_list, MUIM_NList_Clear);
-
-	if (node)
-	{
-		DoMethod(text_list, MUIM_NList_InsertSingleWrap, (ULONG)node->text, MUIV_NList_Insert_Bottom, WRAPCOL0, ALIGN_LEFT);
-	}
-
-	set(text_list, MUIA_NList_Quiet, FALSE);
-}
-
-/**
- * Clear all messages.
- */
-static void error_window_clear(void)
-{
-	struct error_node *node;
-
-	DoMethod(all_errors_list, MUIM_NList_Clear);
-	while ((node = (struct error_node *)list_remove_tail(&error_list)))
-	{
-		if (node->text) free(node->text);
-		free(node);
-	}
-
-}
-
-STATIC ASM SAVEDS VOID error_display(REG(a0,struct Hook *h),REG(a2,Object *obj),REG(a1,struct NList_DisplayMessage *msg))
-{
-	struct error_node *error = (struct error_node*)msg->entry;
-	if (!error)
-	{
-		msg->strings[0] = _("Time");
-		msg->strings[1] = _("Message");
-		return;
-	}
-	msg->strings[0] = sm_get_time_str(error->date);
-	msg->strings[1] = error->text;
-}
-
-/**
- * Initialize the error window.
- */
-static void init_error(void)
-{
-	static struct Hook error_display_hook;
-
-	if (!MUIMasterBase) return;
-
-	init_hook(&error_display_hook, (HOOKFUNC)error_display);
-
-	list_init(&error_list);
-
-	error_wnd = WindowObject,
-		MUIA_Window_ID, MAKE_ID('E','R','R','O'),
-    MUIA_Window_Title, "SimpleMail - Error",
-    WindowContents, VGroup,
-			Child, NListviewObject,
-				MUIA_CycleChain, 1,
-				MUIA_NListview_NList, all_errors_list = NListObject,
-					MUIA_NList_Format, ",",
-					MUIA_NList_DisplayHook2, &error_display_hook,
-					MUIA_NList_Title, TRUE,
-					End,
-				End,
-			Child, BalanceObject, End,
-			Child, NListviewObject,
-				MUIA_Weight, 33,
-				MUIA_CycleChain, 1,
-				MUIA_NListview_NList, text_list = NListObject,
-					MUIA_NList_TypeSelect, MUIV_NList_TypeSelect_Char,
-					End,
-				End,
-			Child, HGroup,
-				Child, clear_button = MakeButton(_("_Clear")),
-				End,
-			End,
-		End;
-
-	if (error_wnd)
-	{
-		DoMethod(App, OM_ADDMEMBER, (ULONG)error_wnd);
-		DoMethod(error_wnd, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, (ULONG)error_wnd, 3, MUIM_Set, MUIA_Window_Open, FALSE);
-		DoMethod(clear_button, MUIM_Notify, MUIA_Pressed, FALSE, (ULONG)clear_button, 3, MUIM_CallHook, (ULONG)&hook_standard, (ULONG)error_window_clear);
-		DoMethod(all_errors_list, MUIM_Notify, MUIA_NList_Active, MUIV_EveryTime, (ULONG)all_errors_list, 3, MUIM_CallHook, (ULONG)&hook_standard, (ULONG)error_select);
-	}
-}
-
-/*****************************************************************************/
+static logg_listener_t error_logg_listener;
 
 /**
  * Add the logg entry to the error list.
@@ -238,6 +142,132 @@ static void error_window_update_logg(void)
 	}
 
 	set(all_errors_list, MUIA_NList_Quiet, FALSE);
+}
+
+/**
+ * Callback for selecting the currently displayed error message.
+ */
+static void error_select(void)
+{
+	int num = xget(all_errors_list, MUIA_NList_Active);
+	struct error_node *node = (struct error_node *)list_find(&error_list,num);
+
+	set(text_list, MUIA_NList_Quiet, TRUE);
+	DoMethod(text_list, MUIM_NList_Clear);
+
+	if (node)
+	{
+		DoMethod(text_list, MUIM_NList_InsertSingleWrap, (ULONG)node->text, MUIV_NList_Insert_Bottom, WRAPCOL0, ALIGN_LEFT);
+	}
+
+	set(text_list, MUIA_NList_Quiet, FALSE);
+}
+
+/**
+ * Called whenever the logg is updated.
+ *
+ * @param userdata
+ */
+static void error_window_logg_update_callback(void *userdata)
+{
+	error_window_update_logg();
+}
+
+/**
+ * Callback that is called when the window open state changes.
+ */
+static void error_window_open_state(void)
+{
+	BOOL error_window_open;
+
+	error_window_open = xget(error_wnd, MUIA_Window_Open);
+
+	if (error_window_open && !error_logg_listener)
+	{
+		error_logg_listener = logg_add_update_listener(error_window_logg_update_callback, NULL);
+	} else if (!error_window_open && error_logg_listener)
+	{
+		logg_remove_update_listener(error_logg_listener);
+		error_logg_listener = NULL;
+	}
+}
+
+/**
+ * Clear all messages.
+ */
+static void error_window_clear(void)
+{
+	struct error_node *node;
+
+	DoMethod(all_errors_list, MUIM_NList_Clear);
+	while ((node = (struct error_node *)list_remove_tail(&error_list)))
+	{
+		if (node->text) free(node->text);
+		free(node);
+	}
+
+}
+
+STATIC ASM SAVEDS VOID error_display(REG(a0,struct Hook *h),REG(a2,Object *obj),REG(a1,struct NList_DisplayMessage *msg))
+{
+	struct error_node *error = (struct error_node*)msg->entry;
+	if (!error)
+	{
+		msg->strings[0] = _("Time");
+		msg->strings[1] = _("Message");
+		return;
+	}
+	msg->strings[0] = sm_get_time_str(error->date);
+	msg->strings[1] = error->text;
+}
+
+/**
+ * Initialize the error window.
+ */
+static void init_error(void)
+{
+	static struct Hook error_display_hook;
+
+	if (!MUIMasterBase) return;
+
+	init_hook(&error_display_hook, (HOOKFUNC)error_display);
+
+	list_init(&error_list);
+
+	error_wnd = WindowObject,
+		MUIA_Window_ID, MAKE_ID('E','R','R','O'),
+    MUIA_Window_Title, "SimpleMail - Error",
+    WindowContents, VGroup,
+			Child, NListviewObject,
+				MUIA_CycleChain, 1,
+				MUIA_NListview_NList, all_errors_list = NListObject,
+					MUIA_NList_Format, ",",
+					MUIA_NList_DisplayHook2, &error_display_hook,
+					MUIA_NList_Title, TRUE,
+					End,
+				End,
+			Child, BalanceObject, End,
+			Child, NListviewObject,
+				MUIA_Weight, 33,
+				MUIA_CycleChain, 1,
+				MUIA_NListview_NList, text_list = NListObject,
+					MUIA_NList_TypeSelect, MUIV_NList_TypeSelect_Char,
+					End,
+				End,
+			Child, HGroup,
+				Child, clear_button = MakeButton(_("_Clear")),
+				End,
+			End,
+		End;
+
+	if (error_wnd)
+	{
+		DoMethod(App, OM_ADDMEMBER, (ULONG)error_wnd);
+		DoMethod(error_wnd, MUIM_Notify, MUIA_Window_Open, MUIV_EveryTime, (ULONG)error_wnd, 3, MUIM_CallHook, (ULONG)&hook_standard, (ULONG)error_window_open_state);
+		DoMethod(error_wnd, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, (ULONG)error_wnd, 3, MUIM_Set, MUIA_Window_Open, FALSE);
+		DoMethod(clear_button, MUIM_Notify, MUIA_Pressed, FALSE, (ULONG)clear_button, 3, MUIM_CallHook, (ULONG)&hook_standard, (ULONG)error_window_clear);
+		DoMethod(all_errors_list, MUIM_Notify, MUIA_NList_Active, MUIV_EveryTime, (ULONG)all_errors_list, 3, MUIM_CallHook, (ULONG)&hook_standard, (ULONG)error_select);
+	}
 }
 
 /*****************************************************************************/

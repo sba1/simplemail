236cb6376750f52f354744ff2d1c59fe7e33bb85
Sebastian Bauer <mail@sebastianbauer.info>
2016-12-03 20:39:50 +0100

    Added simple tool to extract text data from source files.

---
+++ tests/gen-test-data.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+#
+# Generates test data embedded into the source file that is read from stdin
+#
+import re
+import sys
+
+text = sys.stdin.read()
+
+result = re.findall(r'@Test.*@File\s\"(.+)\".*\n(.*){{{\n(.*)}}}', text, re.DOTALL)
+
+for r in result:
+    fn = r[0]
+    prefix = r[1]
+    contents = r[2]
+    # Extract the proper lines, i.e., without the prefix and without the last line
+    lines = [l[len(prefix):] + "\n" for l in contents.split('\n')[:-1]]
+    with open(r[0],'w') as f:
+        f.writelines(lines)

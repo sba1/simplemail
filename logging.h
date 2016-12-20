d7d50ce820c11b03a90d0872d804c8ebb0ac2202
Sebastian Bauer <mail@sebastianbauer.info>
2016-12-20 10:48:55 +0100

    Added missing LAST terminator in the logg() call.
    As it is not really evaluated, this was no real bug for now.

--- logging.h
+++ logging.h
@@ -156,7 +156,7 @@ int logg_line(logg_t logg);
 #define SM_LOG_INFO(level,text) \
 	do { \
 		static const char filename[] __attribute__((used, section("LOGMODULES"))) = "LOGMODULE:" __FILE__;\
-		logg(INFO, 0, __FILE__, __PRETTY_FUNCTION__, __LINE__, text);\
+		logg(INFO, 0, __FILE__, __PRETTY_FUNCTION__, __LINE__, text, LAST);\
 	} while (0)
 
 /*****************************************************************************/

be0afd968796f9bc1a89dc3cd501071e3672249f
Sebastian Bauer <mail@sebastianbauer.info>
2017-01-03 14:49:15 +0100

    Stub unit test for address book functions.

---
+++ tests/addressbook_unittest.c
@@ -0,0 +1,42 @@
+/***************************************************************************
+ SimpleMail - Copyright (C) 2000 Hynek Schlawack and Sebastian Bauer
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include <CUnit/Basic.h>
+
+#include "addressbook.h"
+#include "configuration.h"
+
+/*****************************************************************************/
+
+/* @Test */
+void test_addressbook_simple(void)
+{
+	config_set_user_profile_directory("test-profile");
+
+	CU_ASSERT_EQUAL(load_config(), 1);
+	init_addressbook();
+	cleanup_addressbook();
+	free_config();
+}
+
+/*****************************************************************************/

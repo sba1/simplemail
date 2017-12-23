890d5f0dc5dc690dc33715316c0b12bf7e9074c6
Sebastian Bauer <mail@sebastianbauer.info>
2017-12-23 10:42:28 +0100

    Introduce mail context.

---
+++ mail_context.c
@@ -0,0 +1,43 @@
+/**
+ * @file
+ */
+
+#include "mail_context.h"
+
+#include <stdlib.h>
+
+/**
+ * Create a single mail context.
+ *
+ * @return the pointer to a mail context
+ */
+mail_context *mail_context_create(void)
+{
+	mail_context *c = malloc(sizeof(*c));
+	if (!c)
+	{
+		return NULL;
+	}
+	if (!(c->sp = string_pool_create()))
+	{
+		free(c);
+		return NULL;
+	}
+	return c;
+}
+
+/**
+ * Free the given mail context.
+ *
+ * @param c the context to be freed
+ */
+void mail_context_free(mail_context *c)
+{
+	if (!c)
+	{
+		return;
+	}
+
+	string_pool_delete(c->sp);
+	free(c);
+}

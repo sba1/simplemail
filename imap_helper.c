0f0392b9bfbac195cafe0135cbf07464576a5df4
Sebastian Bauer <mail@sebastianbauer.info>
2018-07-12 23:12:01 +0200

    Move imap_get_remote_mails_handle_answer() into the helper.

--- imap_helper.c
+++ imap_helper.c
@@ -7,6 +7,10 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+#include "debug.h"
+#include "qsort.h"
+#include "support_indep.h"
+
 /******************************************************************************/
 
 char *imap_get_result(char *src, char *dest, int dest_size)
@@ -70,3 +74,121 @@ char *imap_get_result(char *src, char *dest, int dest_size)
 }
 
 /******************************************************************************/
+
+int imap_get_remote_mails_handle_answer(struct connection *conn, char *tag, char *buf, int buf_size, struct remote_mail *remote_mail_array, int num_of_remote_mails)
+{
+	char *line;
+	int success = 0;
+	int needs_to_be_sorted = 0;
+	unsigned int max_uid = 0; /* Max UID discovered so far */
+
+	while ((line = tcp_readln(conn)))
+	{
+		SM_DEBUGF(10,("Server: %s",line));
+		line = imap_get_result(line,buf,buf_size);
+		if (!mystricmp(buf,tag))
+		{
+			line = imap_get_result(line,buf,buf_size);
+			if (!mystricmp(buf,"OK")) success = 1;
+			break;
+		} else
+		{
+			/* untagged */
+			unsigned int msgno;
+			int is_mail = 0;
+			unsigned int uid = 0;
+			unsigned int flags = 0;
+			unsigned int size = 0;
+			char *headers = NULL;
+			char msgno_buf[40];
+			char stuff_buf[768];
+			char cmd_buf[768];
+			char *temp;
+			int i;
+
+			line = imap_get_result(line,msgno_buf,sizeof(msgno_buf));
+			line = imap_get_result(line,cmd_buf,sizeof(cmd_buf));
+			imap_get_result(line,stuff_buf,sizeof(stuff_buf)); /* don't update the line because BODY[HEADER.FIELDS] would be skipped and because it is parsed diffently */
+
+			msgno = (unsigned int)atoi(msgno_buf);
+			temp = stuff_buf;
+
+			for (i=0;i<4;i++)
+			{
+				temp = imap_get_result(temp,cmd_buf,sizeof(cmd_buf));
+				if (!mystricmp(cmd_buf,"UID"))
+				{
+					temp = imap_get_result(temp,cmd_buf,sizeof(cmd_buf));
+					uid = atoi(cmd_buf);
+					is_mail = 1;
+				}
+				else if (!mystricmp(cmd_buf,"FLAGS"))
+				{
+					temp = imap_get_result(temp,cmd_buf,sizeof(cmd_buf));
+					if (strstr(cmd_buf,"\\Seen")) flags |= RM_FLAG_SEEN;
+					if (strstr(cmd_buf,"\\Answered")) flags |= RM_FLAG_ANSWERED;
+					if (strstr(cmd_buf,"\\Flagged")) flags |= RM_FLAG_FLAGGED;
+				}
+				else if (!mystricmp(cmd_buf,"RFC822.SIZE"))
+				{
+					temp = imap_get_result(temp,cmd_buf,sizeof(cmd_buf));
+					size = atoi(cmd_buf);
+				}
+				else if (!mystrnicmp(cmd_buf,"BODY",4))
+				{
+					char *temp_ptr;
+					int todownload;
+
+					if ((temp_ptr = strchr(line,'{'))) /* } - avoid bracket checking problems */
+					{
+						temp_ptr++;
+						todownload = atoi(temp_ptr);
+					} else todownload = 0;
+
+					if (todownload)
+					{
+						int pos = 0;
+
+						if ((headers = (char*)malloc(todownload+1)))
+						{
+							headers[todownload]=0;
+
+							while (todownload)
+							{
+								int dl;
+								dl = tcp_read(conn,headers + pos,todownload);
+								if (dl == -1 || !dl) break;
+								todownload -= dl;
+								pos += dl;
+							}
+						}
+					}
+				}
+			}
+
+			if (msgno <= num_of_remote_mails && msgno > 0 && is_mail)
+			{
+				remote_mail_array[msgno-1].uid = uid;
+				remote_mail_array[msgno-1].flags = flags;
+				remote_mail_array[msgno-1].size = size;
+				remote_mail_array[msgno-1].headers = headers;
+
+				if (uid < max_uid) needs_to_be_sorted = 1;
+				else max_uid = uid;
+			} else
+			{
+				free(headers);
+			}
+		}
+	}
+
+	if (needs_to_be_sorted)
+	{
+		SM_DEBUGF(10,("Sorting remote array\n"));
+
+		#define remote_mail_lt(a,b) ((a->uid < b->uid)?1:0)
+		QSORT(struct remote_mail, remote_mail_array, num_of_remote_mails, remote_mail_lt);
+	}
+
+	return success;
+}
--- imap_helper.h
+++ imap_helper.h
@@ -5,6 +5,29 @@
 #ifndef SM__IMAP_HELPER_H
 #define SM__IMAP_HELPER_H
 
+#include "tcp.h"
+
+#define RM_FLAG_SEEN      (1L<<0)
+#define RM_FLAG_ANSWERED  (1L<<1)
+#define RM_FLAG_FLAGGED	(1L<<2)
+
+struct remote_mail
+{
+	unsigned int uid;
+	unsigned int flags;
+	unsigned int size;
+
+	/* only if headers are requested  */
+	char *headers;
+};
+
+struct local_mail
+{
+	unsigned int uid;
+	unsigned int todel;
+};
+
+
 /**
  * Writes the next word into the dest buffer but not more than dest_size.
  *
@@ -14,5 +37,19 @@
  * @return
  */
 char *imap_get_result(char *src, char *dest, int dest_size);
+
+
+/**
+ * Handle the answer of imap_get_remote_mails().
+ *
+ * @param conn
+ * @param tag
+ * @param buf
+ * @param buf_size
+ * @param remote_mail_array
+ * @param num_of_remote_mails
+ * @return
+ */
+int imap_get_remote_mails_handle_answer(struct connection *conn, char *tag, char *buf, int buf_size, struct remote_mail *remote_mail_array, int num_of_remote_mails);
 
 #endif

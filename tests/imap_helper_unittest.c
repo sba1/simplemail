37fa46a86b29061e74ba613839e1c52a0dbecc1c
Sebastian Bauer <mail@sebastianbauer.info>
2018-07-16 20:32:09 +0200

    Add first test for imap_get_result().

--- tests/imap_helper_unittest.c
+++ tests/imap_helper_unittest.c
@@ -4,3 +4,25 @@
 
 #include <CUnit/Basic.h>
 
+#include "imap_helper.h"
+
+/******************************************************************************/
+
+/* @Test */
+void test_imap_get_result(void)
+{
+	char *res;
+	char dest[200];
+	int dest_size = sizeof(dest);
+
+	res = imap_get_result(" ANSWER1 ANSWER2 (ANSWER3a ANSWER3b)",dest,dest_size);
+	CU_ASSERT_STRING_EQUAL(dest, "ANSWER1");
+
+	res = imap_get_result(res,dest,dest_size);
+	CU_ASSERT_STRING_EQUAL(dest, "ANSWER2");
+
+	res = imap_get_result(res,dest,dest_size);
+	CU_ASSERT_STRING_EQUAL(dest, "ANSWER3a ANSWER3b");
+}
+
+/******************************************************************************/

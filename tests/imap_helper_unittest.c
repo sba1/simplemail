e28c739ce622573748686d6a7e9edd8a96c8e3fb
Sebastian Bauer <mail@sebastianbauer.info>
2018-07-17 20:59:28 +0200

    Also test quotation marks.

--- tests/imap_helper_unittest.c
+++ tests/imap_helper_unittest.c
@@ -15,7 +15,7 @@ void test_imap_get_result(void)
 	char dest[200];
 	int dest_size = sizeof(dest);
 
-	res = imap_get_result(" ANSWER1 ANSWER2 (ANSWER3a ANSWER3b)",dest,dest_size);
+	res = imap_get_result(" ANSWER1 ANSWER2 (ANSWER3a ANSWER3b) \"ANSWER4\"",dest,dest_size);
 	CU_ASSERT_STRING_EQUAL(dest, "ANSWER1");
 
 	res = imap_get_result(res,dest,dest_size);
@@ -23,6 +23,9 @@ void test_imap_get_result(void)
 
 	res = imap_get_result(res,dest,dest_size);
 	CU_ASSERT_STRING_EQUAL(dest, "ANSWER3a ANSWER3b");
+
+	res = imap_get_result(res,dest,dest_size);
+	CU_ASSERT_STRING_EQUAL(dest, "ANSWER4");
 }
 
 /******************************************************************************/

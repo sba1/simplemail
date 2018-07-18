31382b090dfd607848c6b2cf4b820dd54e54fcf0
Sebastian Bauer <mail@sebastianbauer.info>
2018-07-18 20:27:53 +0200

    Also test truncation.

--- tests/imap_helper_unittest.c
+++ tests/imap_helper_unittest.c
@@ -28,4 +28,18 @@ void test_imap_get_result(void)
 	CU_ASSERT_STRING_EQUAL(dest, "ANSWER4");
 }
 
+/* @Test */
+void test_imap_get_result_too_small(void)
+{
+	char *res;
+	char dest[2];
+	int dest_size = sizeof(dest);
+
+	res = imap_get_result(" ANSWER1 ANSWER2",dest,dest_size);
+	CU_ASSERT_STRING_EQUAL(dest, "A");
+
+	res = imap_get_result(res,dest,dest_size);
+	CU_ASSERT_STRING_EQUAL(dest, "A");
+}
+
 /******************************************************************************/

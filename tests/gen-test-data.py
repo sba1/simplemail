2b0c9e39712f3f39f789627094da41543656c935
Sebastian Bauer <mail@sebastianbauer.info>
2018-09-29 18:40:40 +0200

    Add support of an output directory in which the files are created.

--- tests/gen-test-data.py
+++ tests/gen-test-data.py
@@ -2,19 +2,40 @@
 #
 # Generates test data embedded into the source file that is read from stdin
 #
+from __future__ import print_function
+
 import re
+import os.path
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="""
+  Generates test data embedded into the source file that is read from stdin.
+  """)
+
+parser.add_argument('--outdir',  help="""
+  Write the files into the specified output direcory. If this doesn't exists
+  it is creatde.
+  """)
+
+args = parser.parse_args()
+
+outdir = ''
+if args.outdir is not None:
+  outdir = args.outdir
+  if not os.path.exists(args.outdir):
+    os.mkdir(outdir)
 
 text = sys.stdin.read()
 
 result = re.findall(r'@Test[^@.]*@File\s\"([^\"]+)\"\w*\n(.*?){{{\n(.*?)}}}', text, re.DOTALL)
 
 for r in result:
-    fn = r[0]
+    fn = os.path.join(outdir, r[0])
     prefix = r[1]
     contents = r[2]
     # Extract the proper lines, i.e., without the prefix and without the last line
     lines = [l[len(prefix):] + "\n" for l in contents.split('\n')[:-1]]
     sys.stderr.write('Generating {0}\n'.format(fn))
-    with open(r[0],'w') as f:
+    with open(fn,'w') as f:
         f.writelines(lines)

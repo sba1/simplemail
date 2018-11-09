64f7385b11ed46ff2474d0734405131d1f06206f
Sebastian Bauer <mail@sebastianbauer.info>
2018-11-09 20:55:34 +0100

    Add Python script for generating stubs.
    This is work in progress.

---
+++ devtools/gen-stubs.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+#
+# This simple script scans a simple c soure file that contains prototypes of
+# functions to which a function and its actual arguments are passed in order to
+# call them at some subsequent timepoint and generates simple C++-based variants
+# of them that verify the correctness of the types at compile time.
+#
+# (c) 2018 by Sebastian Bauer
+#
+from __future__ import print_function
+
+from pycparser import parse_file, c_ast, c_parser, c_generator
+from pycparser.c_ast import FuncDecl, TypeDecl
+
+import re
+import subprocess
+import tempfile
+
+class FuncDefVisitor(c_ast.NodeVisitor):
+  def visit_Decl(self, node):
+    if isinstance(node.type, FuncDecl):
+      generator = c_generator.CGenerator()
+      return_type = node.type.type
+      return_type_str = generator.visit(return_type)
+
+      print(generator.visit(node))
+      print('{')
+      print("  fprintf(stderr, \"%s called\", __PRETTY_FUNCTION__);")
+      print("  exit(1);")
+      if (not isinstance(return_type, TypeDecl)) or (not return_type_str == 'void'):
+        print('  return 0;')
+      print('}\n')
+
+if __name__ == '__main__':
+  includes = subprocess.check_output("echo '#define __attribute__(x)\n#define __extension__(x)\n#define __asm__(x)\n' | cat - indep-include/* | gcc -DNO_SSL -Dsize_t=int -E -xc - -I indep-include/ | grep -v ^\#", shell=True)
+  includes = re.sub(r'__attribute__\s*\(\(.*[^\)]\)\)','',includes)
+  includes = re.sub(r'__nptr','',includes)
+  includes = re.sub(r'__endptr','',includes)
+  includes = re.sub(r'__extension__','',includes)
+  includes = re.sub(r'__inline','',includes)
+  includes = re.sub(r'__restrict','',includes)
+  includes = re.sub(r'__builtin_va_list','int',includes)
+
+  ast = c_parser.CParser().parse(includes)
+  v = FuncDefVisitor()
+  v.visit(ast)

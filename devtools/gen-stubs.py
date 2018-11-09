9fa279cca220da2f246627bb396a9660b7e9d2f3
Sebastian Bauer <mail@sebastianbauer.info>
2018-11-09 23:32:05 +0100

    Only output stubs for requested functions.

--- devtools/gen-stubs.py
+++ devtools/gen-stubs.py
@@ -9,14 +9,22 @@ from __future__ import print_function
 from pycparser import parse_file, c_ast, c_parser, c_generator
 from pycparser.c_ast import FuncDecl, TypeDecl
 
+import argparse
 import re
 import subprocess
 import tempfile
 
 class FuncDefVisitor(c_ast.NodeVisitor):
+  def __init__(self, functions):
+    # type: (List[str]) -> None
+    self.functions = functions
+
   def visit_Decl(self, node):
     if isinstance(node.type, FuncDecl):
       generator = c_generator.CGenerator()
+      if node.name not in self.functions:
+        return
+
       return_type = node.type.type
       return_type_str = generator.visit(return_type)
 
@@ -29,6 +37,13 @@ class FuncDefVisitor(c_ast.NodeVisitor):
       print('}\n')
 
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('functions', help="The file the contains, line by line, the function for which the stubs shall be generated")
+  args = parser.parse_args()
+
+  with open(args.functions) as f:
+    functions = set([l.strip() for l in f.readlines()])
+#
   includes = subprocess.check_output("echo '#define __attribute__(x)\n#define __extension__(x)\n#define __asm__(x)\n' | cat - indep-include/* | gcc -DNO_SSL -Dsize_t=int -E -xc - -I indep-include/ | grep -v ^\#", shell=True)
   includes = re.sub(r'__attribute__\s*\(\(.*[^\)]\)\)','',includes)
   includes = re.sub(r'__nptr','',includes)
@@ -39,5 +54,5 @@ if __name__ == '__main__':
   includes = re.sub(r'__builtin_va_list','int',includes)
 
   ast = c_parser.CParser().parse(includes)
-  v = FuncDefVisitor()
+  v = FuncDefVisitor(functions)
   v.visit(ast)

66feae3e554c76eae7bd133826f64757b4197485
Sebastian Bauer <mail@sebastianbauer.info>
2018-11-10 20:46:39 +0100

    Do more ncurses related preparations.

--- ncurses/gui_main.c
+++ ncurses/gui_main.c
@@ -1,12 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <ncurses.h>
+
+#include "debug.h"
 #include "gui_main.h"
 
 /*****************************************************************************/
 
+int gui_init(void)
+{
+	atexit(endwin);
+	initscr();
+
+	return 1;
+}
+
+/*****************************************************************************/
+
+static int gui_argc;
+static char **gui_argv;
+
 int gui_parseargs(int argc, char *argv[])
 {
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (!strcmp("--debug"))
+		{
+			debug_set_level(20);
+		}
+	}
 	return 1;
 }
 
@@ -14,14 +39,8 @@ int gui_parseargs(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	if (!simplemail_init())
-	{
-		fprintf(stderr, "SimpleMail initialization failed!\n");
-		return EXIT_FAILURE;
-	}
+	gui_argc = argc;
+	gui_argv = argv;
 
-	simplemail_deinit();
-
-	return EXIT_SUCCESS;
+	return simplemail_main();
 }
-

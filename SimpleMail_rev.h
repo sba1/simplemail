8c8a6613b81c15f37ccc428c8890b4c74aeaec4f
Sebastian Bauer <mail@sebastianbauer.info>
2018-12-24 10:49:27 +0100

    Next version finalization.

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
 #define REVISION	45
-#define DATE		"25.12.2017"
+#define DATE		"24.12.2018"
 #define VERS		"SimpleMail 0.45"
-#define VSTRING		"SimpleMail 0.45 (25.12.2017)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.45 (25.12.2017)"
+#define VSTRING		"SimpleMail 0.45 (24.12.2018)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.45 (24.12.2018)"

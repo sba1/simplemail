e02a4d74fb5400ce236b7bd9e432cdd7c17ed061
Sebastian Bauer <mail@sebastianbauer.info>
2018-12-25 21:29:49 +0100

    Version bump for next version.

--- SimpleMail_rev.h
+++ SimpleMail_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		0
-#define REVISION	45
-#define DATE		"24.12.2018"
-#define VERS		"SimpleMail 0.45"
-#define VSTRING		"SimpleMail 0.45 (24.12.2018)\r\n"
-#define VERSTAG		"\0$VER: SimpleMail 0.45 (24.12.2018)"
+#define REVISION	46
+#define DATE		"25.12.2018"
+#define VERS		"SimpleMail 0.46"
+#define VSTRING		"SimpleMail 0.46 (25.12.2018)\r\n"
+#define VERSTAG		"\0$VER: SimpleMail 0.46 (25.12.2018)"

2fad765caf6291868763c7b066b5ed0067821014
Sebastian Bauer <mail@sebastianbauer.info>
2019-03-14 23:06:53 +0100

    Add read_window_open() dummy.

--- ncurses/readwnd.c
+++ ncurses/readwnd.c
@@ -9,3 +9,10 @@
 void read_refresh_prevnext_button(struct folder *f)
 {
 }
+
+/******************************************************************************/
+
+int read_window_open(const char *folder, struct mail_info *mail, int window)
+{
+	return 0;
+}

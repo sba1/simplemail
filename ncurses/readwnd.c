9b35a3e576a3231e9188712ec77a0eb835abc9d4
Sebastian Bauer <mail@sebastianbauer.info>
2019-03-18 23:38:59 +0100

    Open the read window.

--- ncurses/readwnd.c
+++ ncurses/readwnd.c
@@ -4,6 +4,14 @@
 
 #include "readwnd.h"
 
+#include <ncurses.h>
+#include <panel.h>
+
+/******************************************************************************/
+
+static WINDOW *read_wnd;
+static PANEL *read_panel;
+
 /******************************************************************************/
 
 void read_refresh_prevnext_button(struct folder *f)
@@ -14,5 +22,17 @@ void read_refresh_prevnext_button(struct folder *f)
 
 int read_window_open(const char *folder, struct mail_info *mail, int window)
 {
-	return 0;
+	int w, h;
+
+	getmaxyx(stdscr, h, w);
+	h -= 2;
+
+	read_wnd = newwin(h, w, 0, 0);
+	read_panel = new_panel(read_wnd);
+
+	refresh();
+
+	wrefresh(read_wnd);
+
+	return 1;
 }

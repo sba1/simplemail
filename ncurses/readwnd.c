a97875819dfcc2ba42109d0a2010839f2f11a652
Sebastian Bauer <mail@sebastianbauer.info>
2019-03-19 22:22:18 +0100

    Allow read window to be closed.

--- ncurses/readwnd.c
+++ ncurses/readwnd.c
@@ -4,6 +4,8 @@
 
 #include "readwnd.h"
 
+#include "gui_main_ncurses.h"
+
 #include <ncurses.h>
 #include <panel.h>
 
@@ -11,6 +13,19 @@
 
 static WINDOW *read_wnd;
 static PANEL *read_panel;
+
+static struct gui_key_listener close;
+
+/******************************************************************************/
+
+static void read_window_close_current(void)
+{
+	gui_remove_key_listener(&close);
+
+	hide_panel(read_panel);
+	update_panels();
+	doupdate();
+}
 
 /******************************************************************************/
 
@@ -27,9 +42,19 @@ int read_window_open(const char *folder, struct mail_info *mail, int window)
 	getmaxyx(stdscr, h, w);
 	h -= 2;
 
-	read_wnd = newwin(h, w, 0, 0);
-	read_panel = new_panel(read_wnd);
+	if (!read_wnd)
+	{
+		read_wnd = newwin(h, w, 0, 0);
+		read_panel = new_panel(read_wnd);
+		show_panel(read_panel);
+	} else
+	{
+		show_panel(read_panel);
+		update_panels();
+		doupdate();
+	}
 
+	gui_add_key_listener(&close, 'c', "Close", read_window_close_current);
 	refresh();
 
 	wrefresh(read_wnd);

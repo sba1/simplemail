06408a8893677d5d4d815a54d4496b7d8435a969
Sebastian Bauer <mail@sebastianbauer.info>
2019-08-04 22:04:03 +0200

    Initialize some parts of the compose window.

--- ncurses/composewnd.c
+++ ncurses/composewnd.c
@@ -26,11 +26,51 @@
 
 #include "composewnd.h"
 
+#include "gadgets.h"
+
+/*****************************************************************************/
+
+static struct window compose_win;
+static int compose_win_initialized;
+static int compose_win_removed;
+static struct group compose_group;
+
+static struct key_listener close_listener;
+
+static void compose_window_close_current(void)
+{
+	if (compose_win_initialized)
+	{
+		screen_remove_window(&gui_screen, &compose_win);
+		compose_win_removed = 1;
+	}
+}
+
 /*****************************************************************************/
 
 int compose_window_open(struct compose_args *args)
 {
-	return -1;
+	if (!compose_win_initialized)
+	{
+		/* FIXME: Doesn't work with multiple windows yet */
+		windows_init(&compose_win);
+		screen_add_window(&gui_screen, &compose_win);
+		gadgets_set_extend(&compose_win.g.g, 0, 0, gui_screen.w, gui_screen.h - 2);
+
+		gadgets_init_group(&compose_group);
+
+		gadgets_add(&compose_win.g, &compose_group.g);
+
+		compose_win_initialized = 1;
+
+		windows_add_key_listener(&compose_win, &close_listener, 'c', "Close", compose_window_close_current);
+	} else if (compose_win_removed)
+	{
+		screen_add_window(&gui_screen, &compose_win);
+	}
+	windows_display(&compose_win, &gui_screen);
+
+	return 0;
 }
 
 /*****************************************************************************/

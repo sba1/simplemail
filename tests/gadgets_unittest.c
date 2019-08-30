f058d6ab03dade6418b0dc27c29e1be329ec68f6
Sebastian Bauer <mail@sebastianbauer.info>
2019-08-30 17:51:18 +0200

    Test breaking the line.

--- tests/gadgets_unittest.c
+++ tests/gadgets_unittest.c
@@ -30,4 +30,10 @@ void test_text_edit_enter_simple_text_works(void)
 	CU_ASSERT(contents != NULL);
 	CU_ASSERT_STRING_EQUAL("hoi\n", contents);
 	free(contents);
+
+	te.g.input(&te.g, '\n');
+	contents = gadgets_get_text_edit_contents(&te);
+	CU_ASSERT(contents != NULL);
+	CU_ASSERT_STRING_EQUAL("ho\ni\n", contents);
+	free(contents);
 }

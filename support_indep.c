5ec840686de12d7fe0be7d262163e13fa72e1298
Sebastian Bauer <mail@sebastianbauer.info>
2019-09-07 12:21:48 +0200

    Move naive exhaustive wrap_text_nicely() into the test.
    This way, we are able to confirm new test results.

--- support_indep.c
+++ support_indep.c
@@ -549,70 +549,6 @@ void wrap_text(char *text, int border)
 
 /*****************************************************************************/
 
-/**
- * Add one to the given bitstring.
- */
-static void add_one(char *bs, int len)
-{
-	int i;
-
-	for (i = len - 1; i >= 0; i--)
-	{
-		if (bs[i] == 0)
-		{
-			bs[i] = 1;
-			return;
-		}
-
-		bs[i] = 0;
-	}
-}
-
-/*#define DEBUG_WRAP_LINE_NICELY */
-
-/*****************************************************************************/
-
-/**
- * Determine the cost of the given breakpoint configuration.
- *
- * @param pos the position of the words to be wrapped
- * @param bp vector of breakpoint indicators
- * @param vlen the length of both vectors.
- * @param mlen the maximal allowed length of one line
- *
- * @return the costs
- */
-static unsigned int breakpoint_costs(const int *pos, const char *bp, int vlen, int mlen)
-{
-	unsigned int cost = 0;
-
-	/* initially, last breakpoint is before first word */
-	int last_bp = 0;
-
-	/* <= because the arrays carry one more element and we don't want
-	 * to special case that yet */
-	for (int j = 1; j < vlen; j++)
-	{
-		if (bp[j])
-		{
-			int line_len = pos[j] - pos[last_bp] - 1; /* don't count the trailing space */
-
-			if (line_len > mlen)
-			{
-				cost = 1000000;
-			} else
-			{
-				cost += (mlen - line_len) * (mlen - line_len);
-			}
-			/* Remember this break point as new last */
-			last_bp = j;
-		}
-	}
-	return cost;
-}
-
-/*****************************************************************************/
-
 void wrap_line_nicely(char *text, int border)
 {
 	unsigned char *buf = (unsigned char *)text;
@@ -624,7 +560,6 @@ void wrap_line_nicely(char *text, int border)
 	int i,j;
 
 	int *pos; /* position vector */
-	char *bp; /* breakpoint vector */
 	int *costs;
 	int *prev_bp; /* index to the previous break point (together, they form a line) */
 
@@ -659,35 +594,25 @@ void wrap_line_nicely(char *text, int border)
 		return;
 	}
 
-	if (!(bp = (char *)malloc((words + 1) * sizeof(bp[0]))))
-	{
-		free(pos);
-		return;
-	}
-
 	if (!(prev_bp = (int *)malloc((words + 1) * sizeof(prev_bp[0]))))
 	{
 		free(pos);
-		free(bp);
 		return;
 	}
 
 	if (!(costs = (int *)malloc((words + 1) * sizeof(costs[0]))))
 	{
 		free(prev_bp);
-		free(bp);
 		free(pos);
 		return;
 	}
 
 	/* Fix start and end */
 	pos[0] = -1; /* this "emulates" a space before the first word */
-	bp[0] = 0; /* will be misused as an indicator to leave (if it is 1) */
 	prev_bp[0] = 0;
 	costs[0] = 0;
 
 	pos[words] = len;
-	bp[words] = 1; /* this forces a breakpoint at the end of the line */
 
 	word = 1;
 
@@ -697,11 +622,10 @@ void wrap_line_nicely(char *text, int border)
 		if (isspace(buf[i]))
 		{
 			pos[word] = i;
-			bp[word] = 0;
 			word++;
 		}
 	}
-#if 1
+
 	for (j = 1; j <= words; j++)
 	{
 		int min_cost_i = -1;
@@ -740,56 +664,12 @@ void wrap_line_nicely(char *text, int border)
 		j = prev_bp[j];
 	}
 
-#else
-	/* Enumerate and evaluate. This is a naive implementation and not usable for
-	 * even low number of words.
-	 */
-	min_cost = 1000000;
-	while (!bp[0])
-	{
-		unsigned int cost;
-
-#ifdef DEBUG_WRAP_LINE_NICELY
-		printf("\n");
-		for (int j = 1; j <= bps; j++)
-		{
-			printf("%d ", bp[j]);
-		}
-		printf("\n");
-#endif
-
-		cost = breakpoint_costs(pos, bp, bps + 2 /* head and tail */, border);
-
-		if (cost < min_cost)
-		{
-#ifdef DEBUG_WRAP_LINE_NICELY
-			printf("NEW MIN\n");
-#endif
-			min_cost = cost;
-
-			/* Now set breakpoints corresponding to the current minimal config */
-			for (int j = 1; j <= bps; j++)
-			{
-				text[pos[j]] = bp[j]?'\n':' ';
-			}
-		}
-
-#ifdef DEBUG_WRAP_LINE_NICELY
-		printf("cost: %d\n", cost);
-#endif
-
-		/* Next combination, note that if the index will become 1, we'll leave */
-		add_one(bp, bps + 1);
-	}
-#endif
-
 #ifdef DEBUG_WRAP_LINE_NICELY
 	printf("OUTPUT\n%s\n", text);
 #endif
 	free(costs);
 	free(prev_bp);
 	free(pos);
-	free(bp);
 }
 
 /**
--- tests/support_indep_unittest.c
+++ tests/support_indep_unittest.c
@@ -16,6 +16,7 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ***************************************************************************/
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -116,6 +117,191 @@ void test_wrap_text(void)
 }
 
 /********************************************************/
+
+/**
+ * Add one to the given bitstring.
+ */
+static void add_one(char *bs, int len)
+{
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (bs[i] == 0)
+		{
+			bs[i] = 1;
+			return;
+		}
+
+		bs[i] = 0;
+	}
+}
+
+/*#define DEBUG_WRAP_LINE_NICELY */
+
+/*****************************************************************************/
+
+/**
+ * Determine the cost of the given breakpoint configuration.
+ *
+ * @param pos the position of the words to be wrapped
+ * @param bp vector of breakpoint indicators
+ * @param vlen the length of both vectors.
+ * @param mlen the maximal allowed length of one line
+ *
+ * @return the costs
+ */
+static unsigned int breakpoint_costs(const int *pos, const char *bp, int vlen, int mlen)
+{
+	unsigned int cost = 0;
+
+	/* initially, last breakpoint is before first word */
+	int last_bp = 0;
+
+	/* <= because the arrays carry one more element and we don't want
+	 * to special case that yet */
+	for (int j = 1; j < vlen; j++)
+	{
+		if (bp[j])
+		{
+			int line_len = pos[j] - pos[last_bp] - 1; /* don't count the trailing space */
+
+			if (line_len > mlen)
+			{
+				cost = 1000000;
+			} else
+			{
+				cost += (mlen - line_len) * (mlen - line_len);
+			}
+			/* Remember this break point as new last */
+			last_bp = j;
+		}
+	}
+	return cost;
+}
+
+/**
+ * Naive version of wrap_line_nicely so that we are able
+ * to compare results.
+ */
+static void wrap_line_nicely_naive(char *text, int border)
+{
+	unsigned char *buf = (unsigned char *)text;
+	int min_cost;
+	int words;
+	int word;
+	int bps;
+	int len;
+	int i,j;
+
+	int *pos; /* position vector */
+	char *bp; /* breakpoint vector */
+
+	border--;
+
+#ifdef DEBUG_WRAP_LINE_NICELY
+	printf("INPUT\n%s\n", text);
+#endif
+	len = strlen(text);
+
+	/* First, convert all new lines to spaces (just in case), and count number
+	 * of words */
+	bps = 0;
+	for (i = 0; i < len; i++)
+	{
+		unsigned char c;
+
+		c = buf[i];
+
+		if (c == '\n')
+		{
+			buf[i] = ' ';
+		}
+
+		bps += !!isspace(buf[i]);
+	}
+	words = bps + 1;
+
+	/* Allocate our vectors */
+	if (!(pos = (int *)malloc((words + 1) * sizeof(pos[0]))))
+	{
+		return;
+	}
+
+	if (!(bp = (char *)malloc((words + 1) * sizeof(bp[0]))))
+	{
+		free(pos);
+		return;
+	}
+
+	/* Fix start and end */
+	pos[0] = -1; /* this "emulates" a space before the first word */
+	bp[0] = 0; /* will be misused as an indicator to leave (if it is 1) */
+
+	pos[words] = len;
+	bp[words] = 1; /* this forces a breakpoint at the end of the line */
+
+	word = 1;
+
+	/* Fill vectors */
+	for (i = 0; i < len; i++)
+	{
+		if (isspace(buf[i]))
+		{
+			pos[word] = i;
+			bp[word] = 0;
+			word++;
+		}
+	}
+
+	/* Enumerate and evaluate. This is a naive implementation and not usable for
+	 * even low number of words.
+	 */
+	min_cost = 1000000;
+	while (!bp[0])
+	{
+		unsigned int cost;
+
+#ifdef DEBUG_WRAP_LINE_NICELY
+		printf("\n");
+		for (int j = 1; j <= bps; j++)
+		{
+			printf("%d ", bp[j]);
+		}
+		printf("\n");
+#endif
+
+		cost = breakpoint_costs(pos, bp, bps + 2 /* head and tail */, border);
+
+		if (cost < min_cost)
+		{
+#ifdef DEBUG_WRAP_LINE_NICELY
+			printf("NEW MIN\n");
+#endif
+			min_cost = cost;
+
+			/* Now set breakpoints corresponding to the current minimal config */
+			for (int j = 1; j <= bps; j++)
+			{
+				text[pos[j]] = bp[j]?'\n':' ';
+			}
+		}
+
+#ifdef DEBUG_WRAP_LINE_NICELY
+		printf("cost: %d\n", cost);
+#endif
+
+		/* Next combination, note that if the index will become 1, we'll leave */
+		add_one(bp, bps + 1);
+	}
+
+#ifdef DEBUG_WRAP_LINE_NICELY
+	printf("OUTPUT\n%s\n", text);
+#endif
+	free(pos);
+	free(bp);
+}
+
 
 /* @Test */
 void test_wrap_line_nicely(void)

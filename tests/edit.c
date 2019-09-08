816a85a3d1e24b7bb5ff8b0552cae346c3a786e9
Sebastian Bauer <mail@sebastianbauer.info>
2019-09-08 19:14:30 +0200

    Add text gadget.

--- tests/edit.c
+++ tests/edit.c
@@ -32,12 +32,25 @@ static void gui_segf_handler (int signo)
 int main(int argc, char **argv)
 {
 	struct screen scr;
+	struct window win;
+	struct text_edit te;
 
 	signal(SIGSEGV, gui_segf_handler);
 
 	atexit(edit_exit);
 
 	screen_init(&scr);
+
+	windows_init(&win);
+	gadgets_set_extend(&win.g.g, 0, 0, scr.w, scr.h);
+	screen_add_window(&scr, &win);
+
+	gadgets_init_text_edit(&te);
+	gadgets_set_extend(&te.g, 0, 0, scr.w, scr.h);
+	gadgets_add(&win.g, &te.g);
+
+	windows_activate_gadget(&win, &te.g);
+	windows_display(&win, &scr);
 
 	while(!screen_handle(&scr))
 	{
